c23baccd3dc24b619c88fa15131c8fdf06a10789
Chris Glass <tribaal@gmail.com>
2011-02-02 21:36:09 +0100

    Renamed price_modifiers to price, with a new modifiers submodule Created first test skeleton for the shop!

---
+++ shop/prices/modifiers/taxes_percentage.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from shop.prices.price_modifiers_base import BasePriceModifier
+
+class TaxPercentageModifier(BasePriceModifier):
+    '''
+    A basic Tax calculator: it simply adds a taxes field to the order, 
+    and makes it a fixed percentage of the subtotal.
+    
+    Obviously, this will have to be put in a setting later, but for now
+    it will just be a constant.
+    '''
+    
+    TAX_PERCENTAGE = 7.6 # That's VAT
+    
+    def add_extra_cart_price_field(self, cart):
+        '''
+        Add a field on cart.extra_price_fields:
+        
+        >>> cart.extra_price_fields.update({'Taxes total': 19.00})
+        '''
+        taxes = (self.TAX_PERCENTAGE/100) * cart.subtotal_price
+        cart.extra_price_fields.update({'Taxes total':taxes})
+        cart.update()
---
+++ shop/tests/cart.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+from __future__ import with_statement
+
+class CartTestCase(TestCase):
+    
+    def test_01_empty_cart_costs_0(self):
+        pass
+    
+    def test_02_one_object_no_modifiers(self):
+        pass
+    
+    def test_03_one_object_one_modifier(self):
+        pass
