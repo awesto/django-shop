4cfa70e9c4a6db307c9db4630941f855821d8d2d
Chris Glass <tribaal@gmail.com>
2011-02-02 23:01:01 +0100

    Changed a bit how the price modifiers base class looks like

--- shop/prices/price_modifiers_base.py
+++ shop/prices/price_modifiers_base.py
@@ -10,7 +10,24 @@ class BasePriceModifier():
     It allows to implement Taxes and rebates / bulk prices in an elegant manner
     '''
     def process_cart(self, cart):
-        pass
-    
+        self.add_extra_cart_price_field(cart)
+        
     def process_cart_item(self, cart_item):
-        pass
+        '''
+        Do something fancy here, like calculate size of items or whatever
+        '''
+        self.add_extra_cart_item_price_field(cart_item)
+    
+    def add_extra_cart_item_price_field(self, cart_item):
+        '''
+        Add a field on cart_item.extra_price_fields:
+        
+        >>> cart_item.extra_price_fields.update({'taxes': 9.00})
+        '''
+    
+    def add_extra_cart_price_field(self, cart):
+        '''
+        Add a field on cart.extra_price_fields:
+        
+        >>> cart.extra_price_fields.update({'Taxes total': 19.00})
+        '''
