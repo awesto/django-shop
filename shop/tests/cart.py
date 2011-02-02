becadc5b5c8ef87edbf1e02fdbee21bc1175634d
Chris Glass <tribaal@gmail.com>
2011-02-02 23:02:39 +0100

    Tests breadcrumbs... work in progress

--- shop/tests/cart.py
+++ shop/tests/cart.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
-from unittest import TestCase
 from __future__ import with_statement
+from shop.models.productmodel import Product
+from shop.tests.utils.context_managers import SettingsOverride
+from unittest import TestCase
 
 class CartTestCase(TestCase):
     
     def test_01_empty_cart_costs_0(self):
-        pass
-    
+        with SettingsOverride():
+            product = Product()
+            
+            product.name = "TestPrduct"
+            product.slug = "TestPrduct"
+            product.short_description = "TestPrduct"
+            product.long_description = "TestPrduct"
+            product.active = True
+            product.base_price = '12.0'
+            
+            product.save()
+            
+            
     def test_02_one_object_no_modifiers(self):
         pass
     
