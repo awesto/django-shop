f3b5ef5ea0feca47ca03ceeab23031358785054f
Chris Glass <tribaal@gmail.com>
2011-02-03 21:10:05 +0100

    Modified order model to handle storing of extra price fields

--- shop/models/ordermodel.py
+++ shop/models/ordermodel.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from shop.util.fields import CurrencyField
+from django.core.management.validation import max_length
 
 STATUS_CODES = (
     (1, 'Processing'), # User still checking out the contents
     (2, 'Confirmed'), # Contents are valid, now we can handle payment etc...
     (3, 'Completed'), # Everything is fine, only need to send the products
 )
+
+class OrderManager(models.Manager):
+    
+    def create_from_cart(self, cart):
+        '''
+        This creates a new Order object (and all the rest) from a passed Cart 
+        object.
+        
+        Specifically, it creates an Order with corresponding OrderItems and
+        eventually corresponding ExtraPriceFields
+        
+        '''
 
 class Order(models.Model):
     '''
@@ -20,6 +33,8 @@ class Order(models.Model):
     
     status = models.IntegerField(choices=STATUS_CODES)
     
+    order_subtotal = CurrencyField()
+    order_total = CurrencyField()
     
     shipping_cost = CurrencyField()
     
@@ -42,7 +57,41 @@ class Order(models.Model):
         app_label = 'shop'
 
 class OrderItem():
+    '''
+    A line Item for an order.
+    '''
+    
+    product_name = models.CharField(max_length=255)
+    unit_price = CurrencyField()
+    quantity = models.IntegerField()
+    
+    line_subtotal = CurrencyField()
+    line_total = CurrencyField()
+    
+    class Meta:
+        app_label = 'shop'
+        
+class ExtraOrderPriceField(models.Model):
+    '''
+    This will make Cart-provided extra price fields persistent since we want
+    to "snapshot" their statuses at the time when the order was made
+    '''
+    order = models.ForeignKey(Order)
+    label = models.CharField(max_length=255)
+    value = CurrencyField()
     
     class Meta:
         app_label = 'shop'
     
+class ExtraOrderItemPriceField(models.Model):
+    '''
+    This will make Cart-provided extra price fields persistent since we want
+    to "snapshot" their statuses at the time when the order was made
+    '''
+    order_item = models.ForeignKey(OrderItem)
+    label = models.CharField(max_length=255)
+    value = CurrencyField()
+    
+    class Meta:
+        app_label = 'shop'
+        
