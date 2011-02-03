df6a32099ee26a9aab32a95158a14896fd175797
Chris Glass <christopher.glass@divio.ch>
2011-02-03 10:54:35 +0100

    Fixed del statements and added default of 0.00 if no default is specified

--- shop/util/fields.py
+++ shop/util/fields.py
@@ -9,11 +9,14 @@ from django.db.models.fields import DecimalField
 class CurrencyField(DecimalField):
     '''
     A CurrencyField is simply a subclass of DecimalField with a fixed format:
-    max_digits = 12 and decimal_places=2
+    max_digits = 12, decimal_places=2, and defaults to 0.00
     '''
     def __init__(self, **kwargs):
         if kwargs.get('max_digits'):
-            del kwargs['max_digits']
+            delattr(kwargs,'max_digits')
         if kwargs.get('decimal_places'):
-            del kwargs['decimal_places']
-        return super(CurrencyField,self).__init__(max_digits=12,decimal_places=2,**kwargs)
+            delattr(kwargs,'decimal_places')
+        default = kwargs.get('default', '0.00') # get "default" or 0.00
+        if kwargs.get('default'):
+            delattr(kwargs,'default')
+        return super(CurrencyField,self).__init__(max_digits=12,decimal_places=2,default=default,**kwargs)
