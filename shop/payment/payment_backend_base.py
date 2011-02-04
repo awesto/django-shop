dc0c933390db8729f5e153fd7615ab252f30167b
Chris Glass <christopher.glass@divio.ch>
2011-02-04 13:27:46 +0100

    Added extra documentation

--- shop/payment/payment_backend_base.py
+++ shop/payment/payment_backend_base.py
@@ -2,15 +2,25 @@
 
 class BasePaymentBackend(object):
     '''
-    Needs a lot more work...
+    This is the base class for all payment backends to implement.
+    
+    The goal is to be able to register a few payment modules, and let one of 
+    them be selected at runtime by the shopper.
     '''
     def process_order(self,order):
         '''
-        Processes payment for the supplied Order
+        Processes payment for the supplied Order:
+        
+        Gather all necessary information from the Order object, and
+        pass them to the payment backend (Paypal, whatever)
+        
         '''
         
-    def process_order_item(self,order_item):
-        '''
-        Processes payment for the supplied Order_item.
         
+    def get_urls(self):
+        '''
+        Return a set of patterns() or urls() to hook to the site's main url
+        resolver.
+        This allows payment systems to register urls for callback, or to 
+        maintain a set of own views / templates
         '''
