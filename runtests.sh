27acf46fe867148c656191d421327941d48ac4d4
Chris Glass <tribaal@gmail.com>
2011-02-07 22:45:34 +0100

    Added a --with-coverage command line option to the test runner.

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-pushd .
-cd tests/testapp
-python manage.py test shop
+args=("$@")
+num_args=${#args[@]}
+index=0
 
-popd
+coverage=false
+
+while [ "$index" -lt "$num_args" ]
+do
+	case "${args[$index]}" in
+		"--with-coverage")
+			coverage=true
+			;;
+	esac
+let "index = $index + 1"
+done
+
+if [ $coverage == true ]; then
+	pushd .
+	cd tests/testapp
+	coverage run manage.py test shop
+	coverage html
+	x-www-browser htmlcov/index.html
+	popd
+
+else
+
+	# the default case...
+	pushd .
+	cd tests/testapp
+	python manage.py test shop
+	popd
+
+fi
