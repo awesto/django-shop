e6fa1d292d0f1251f63e1d9f94e8c3ee323019d9
Chris Glass <tribaal@gmail.com>
2011-02-12 22:04:32 +0100

    added --with-docs command line option to build the docs and display them in the default browser

--- runtests.sh
+++ runtests.sh
@@ -5,10 +5,14 @@ num_args=${#args[@]}
 index=0
 
 coverage=false
+documentation=false
 
 while [ "$index" -lt "$num_args" ]
 do
 	case "${args[$index]}" in
+		"--with-docs")
+			documentation=true
+			;;
 		"--with-coverage")
 			coverage=true
 			;;
@@ -32,4 +36,12 @@ else
 	python manage.py test shop
 	popd
 
+fi
+
+if [ $documentation == true ]; then
+	pushd .
+	cd docs/
+	make html
+	x-www-browser _build/html/index.html
+	popd
 fi
