a386a1df3d3c5d8383ba5ee4241445947d053750
Chris Glass <tribaal@gmail.com>
2011-02-12 16:24:25 +0100

    Added a generic class based view for product detail

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, include, url
+from shop.models.productmodel import Product
+from shop.views import ShopDetailView
 from shop.views.cart import CartDetails
 
 # Loop through payment backends and mount the modules in pay/
@@ -7,4 +9,8 @@ urlpatterns = patterns('',
     (r'^pay/$', include('shop.payment.urls')),
     (r'^ship/$', include('shop.shipping.urls')),
     url(r'^cart/$', CartDetails.as_view()),
+    url(r'^product/(?P<pk>[0-9]+)/$', 
+        ShopDetailView.as_view(model=Product, 
+                               template_name="shop/product/product_detail.html")
+                               ),
 )
