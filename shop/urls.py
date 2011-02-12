8ca697bdf3793f06b31a403df51784f36f0afdd9
Chris Glass <tribaal@gmail.com>
2011-02-12 16:42:54 +0100

    Added product list (all products), and product details page views & templates

--- shop/urls.py
+++ shop/urls.py
@@ -1,16 +1,23 @@
 #-*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, include, url
 from shop.models.productmodel import Product
-from shop.views import ShopDetailView
+from shop.views import ShopDetailView, ShopListView
 from shop.views.cart import CartDetails
 
 # Loop through payment backends and mount the modules in pay/
 urlpatterns = patterns('',
     (r'^pay/$', include('shop.payment.urls')),
     (r'^ship/$', include('shop.shipping.urls')),
+    
     url(r'^cart/$', CartDetails.as_view()),
-    url(r'^product/(?P<pk>[0-9]+)/$', 
+    
+    url(r'^product/(?P<slug>[0-9A-Za-z-_.//]+)/$',
         ShopDetailView.as_view(model=Product, 
-                               template_name="shop/product/product_detail.html")
-                               ),
+                               template_name="shop/product/product_detail.html"),
+        name='product_detail'
+        ),
+    url(r'^products/$',
+        ShopListView.as_view(model=Product, template_name="shop/product/product_list.html"),
+        name='product_list'
+        ),
 )
