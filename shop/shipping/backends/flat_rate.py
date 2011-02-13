dc3799a7e37c3545a4aa07c86707f05b27488cff
Chris Glass <tribaal@gmail.com>
2011-02-13 15:30:13 +0100

    Made the example shipping backend work with the new API

--- shop/shipping/backends/flat_rate.py
+++ shop/shipping/backends/flat_rate.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from decimal import Decimal
 from django.conf import settings
 from shop.shipping.shipping_backend_base import BaseShippingBackend
 
@@ -8,5 +9,9 @@ class FlatRateShipping(BaseShippingBackend):
     This is just an example of a possible flat-rate shipping module, that 
     charges a flat rate defined in settings.SHOP_SHIPPING_FLAT_RATE
     '''
+    
     def process_order(self,order):
-        order.shipping_cost = settings.SHOP_SHIPPING_FLAT_RATE
+        self.shop.add_shipping_costs(self.shop.get_order(), 
+                                     'Flat shipping',
+                                     Decimal(settings.SHOP_SHIPPING_FLAT_RATE))
+        
