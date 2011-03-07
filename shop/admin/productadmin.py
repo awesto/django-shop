b632b1663b49fa0c85d68efe450fb25209ba0084
Chris Glass <christopher.glass@divio.ch>
2011-03-07 16:05:12 +0100

    Added Options and OptionGroups to the admin. It's starting to be crowded...

--- shop/admin/productadmin.py
+++ shop/admin/productadmin.py
@@ -1,10 +1,11 @@
 #-*- coding: utf-8 -*-
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
-from shop.models.productmodel import Category
-
+from shop.models.productmodel import Category, OptionGroup, Option
 
 class CategoryAdmin(ModelAdmin):
     pass
 admin.site.register(Category, CategoryAdmin)
 
+admin.site.register(Option)
+admin.site.register(OptionGroup)
