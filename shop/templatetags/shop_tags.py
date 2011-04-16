2d87b67682fa7081bf1299033efd2b34a0d5b171
Bojan Mihelac <bmihelac@mihelac.org>
2011-04-16 09:14:45 +0200

    added Order templatetag

--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django import template
+
 from classytags.helpers import InclusionTag
+from classytags.core import Options
+from classytags.arguments import Argument
 
 from shop.util.cart import get_or_create_cart
 
@@ -20,3 +23,19 @@ class Cart(InclusionTag):
             'cart': cart
         } 
 register.tag(Cart)
+
+
+class Order(InclusionTag):
+    """
+    Inclusio tag for displaying order.
+    """
+    template = 'shop/templatetags/_order.html'
+    options = Options(
+        Argument('order', resolve=True),
+        )
+
+    def get_context(self, context, order):
+        return {
+            'order': order
+        } 
+register.tag(Order)
