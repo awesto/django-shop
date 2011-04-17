438e96b244ac3f00db93c97adfdc3da196116837
Chris Glass <tribaal@gmail.com>
2011-04-17 21:59:38 +0200

    Fixed urls not using reverse...

--- shop/payment/api.py
+++ shop/payment/api.py
@@ -8,6 +8,7 @@ from decimal import Decimal
 from django.http import HttpResponseRedirect
 from shop.models.ordermodel import OrderPayment
 from shop.shop_api import ShopAPI
+from django.core.urlresolvers import reverse
 
 class PaymentAPI(ShopAPI):
     """
@@ -51,12 +52,12 @@ class PaymentAPI(ShopAPI):
         is finished i.e. The payment has been processed from a user perspective
         This will redirect to the "Thanks for your order" page.
         """
-        return HttpResponseRedirect('thank_you_for_your_order')
+        return HttpResponseRedirect(reverse('thank_you_for_your_order'))
     
     def get_cancel_url(self):
         """
         A helper for backends to let them redirect to a generic "order was 
         cancelled" URL of their choosing. 
         """
-        return HttpResponseRedirect('checkout_payment')
+        return HttpResponseRedirect(reverse('checkout_payment'))
     
