ec962bd46db7d1cc43fce71f17a31ee4cfac41a5
Justin Steward <althalus87@gmail.com>
2011-05-03 22:34:52 +1000

    Add __unicode__ method to country.

--- shop/models/clientmodel.py
+++ shop/models/clientmodel.py
@@ -29,6 +29,9 @@ class Client(models.Model):
 
 class Country(models.Model):
     name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return u'%s' % self.name
     
     class Meta:
         app_label = 'shop'
