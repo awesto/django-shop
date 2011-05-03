68b0de4eb33bf489c03f7ddd76fc0f19100cb17b
Justin Steward <althalus87@gmail.com>
2011-05-04 08:12:28 +1000

    split out getting billing/shipping details to own methods, beginnings of a logical POST view. add CSRF to template

--- shop/views/checkout.py
+++ shop/views/checkout.py
@@ -67,6 +67,80 @@ class ThankYouView(ShopTemplateView):
 
 class ShippingBillingView(ShopTemplateView):
     template_name = 'shop/checkout/billingshipping.html'
+
+    def _get_billing_client(self):
+        """
+        If we already have the client, return that (don't duplicate effort)
+        Otherwise, try and grab the client from the database. Failing that,
+        create a new client for the current user.
+        """
+        client = getattr(self, '_client', None)
+        if not client:
+            try:
+                client = Client.objects.get(user=self.request.user)
+            except Client.DoesNotExist:
+                client = Client()
+                client.user = self.request.user
+                client.save()
+            self._client = client
+        return client
+
+
+    def _get_shipping_form(self):
+        """
+        Serves the same purpose as _get_billing_client(). This one for shipping
+        address details.
+        """
+        client = self._get_billing_client()
+        form = getattr(self, '_shipping_address_form', None)
+        if form:
+            pass
+        elif self.request.method == "POST":
+            form = AddressForm(self.request.POST, prefix="ship")
+        else:
+            try:
+                shipping_address = client.shipping_address()
+            except:
+                shipping_address = Address()
+                shipping_address.client = client
+                shipping_address.is_shipping = True
+            form = AddressForm(instance=shipping_address, prefix="ship")
+        self._shipping_address_form = form
+        return form
+            
+    def _get_billing_form(self):
+        """
+        Serves the same purpose as _get_billing_client(). This one for billing
+        address details.
+        """
+        client = self._get_billing_client()
+        form = getattr(self, '_billing_address_form', None)
+        if form:
+            pass
+        elif self.request.method == "POST":
+            form = AddressForm(self.request.POST, prefix="bill")
+        else:
+            try:
+                billing_address = client.billing_address()
+            except:
+                billing_address = Address()
+                billing_address.client = client
+                billing_address.is_billing = True
+            form = AddressForm(instance=billing_address, prefix="bill")
+        self._billing_address_form = form
+        return form
+            
+
+    def post(self, *args, **kwargs):
+        """ Called when view is POSTed """
+        shipping_form = self._get_shipping_form
+        billing_form = self._get_billing_form
+        if shipping_form.is_valid() and billing_form.is_valid():
+            shipping_address = shipping_form.save()
+            billing_address = billing_form.save()
+    
+        return self.get(self, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         """
         This overrides the context from the normal template view
@@ -74,29 +148,10 @@ class ShippingBillingView(ShopTemplateView):
         ctx = super(ShippingBillingView, self).get_context_data(**kwargs)
         payment_modules_list = backends_pool.get_payment_backends_list()
         shipping_modules_list = backends_pool.get_shipping_backends_list()
+        request = self.request
 
-        try:
-            client = Client.objects.get(user=self.request.user)
-        except Client.DoesNotExist:
-            client = Client()
-            client.user = self.request.user
-            client.save()
-
-        try:
-            shipping_address = client.shipping_address()
-        except:
-            shipping_address = Address()
-            shipping_address.client = client
-            shipping_address.is_shipping = True
-        try:
-            billing_address = client.billing_address()
-        except:
-            billing_address = Address()
-            billing_address.client = client
-            billing_address.is_billing = True
-
-        shipping_form = AddressForm(instance=shipping_address)
-        billing_form = AddressForm(instance=billing_address)
+        shipping_form = self._get_shipping_form()
+        billing_form = self._get_billing_form()
         ctx.update({'shipping_address':shipping_form,'billing_address':billing_form})
         
         select = {}
