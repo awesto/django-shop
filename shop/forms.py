c409dff966a55492cc26f929cea7cd115d658fcf
Justin Steward <althalus87@gmail.com>
2011-05-04 14:18:36 +1000

    A form that provides backend choices (Maybe we want radio buttons instead of seelct?)

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from shop.models.clientmodel import *
+from shop.backends_pool import backends_pool
 
 class AddressForm(forms.ModelForm):
     class Meta:
@@ -9,3 +10,13 @@ class AddressForm(forms.ModelForm):
             'is_billing':forms.HiddenInput(),
             'client':forms.HiddenInput(),
         }
+
+def _get_shipping_backends():
+    shipping_backends = backends_pool.get_shipping_backends_list()
+    return tuple([(x.url_namespace, x.backend_name) for x in shipping_backends])
+def _get_billing_backends():
+    billing_backends = backends_pool.get_payment_backends_list()
+    return tuple([(x.url_namespace, x.backend_name) for x in billing_backends])
+class BillingShippingForm(forms.Form):
+    shipping_method = forms.ChoiceField(choices=_get_shipping_backends())
+    payment_method = forms.ChoiceField(choices=_get_billing_backends())
