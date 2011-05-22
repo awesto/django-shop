9b8955e70c5cb3c372a6bf45d460e39925fe64fb
Justin <justin@justinsteward.com>
2011-05-22 14:48:03 +1000

    make payment urls do the same as shipping urls

--- shop/payment/urls.py
+++ shop/payment/urls.py
@@ -7,7 +7,7 @@ http://www.example.com/shop/pay/paypal
 http://www.example.com/shop/pay/pay-on-delivery
 ...
 """
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import patterns, include
 from shop.backends_pool import backends_pool
 
 
@@ -16,4 +16,9 @@ urlpatterns = patterns('')
 # For every backend defined in the backend pool, load all the URLs it defines
 # in its get_urls() method.
 for backend in backends_pool.get_payment_backends_list():
-    urlpatterns = backend.get_urls() + urlpatterns
+    regexp = "^%s/" % backend.url_namespace
+    urls = backend.get_urls()
+    patterns = patterns('',
+        (regexp, include(backend.get_urls()))
+    )
+    urlpatterns = patterns + urlpatterns
