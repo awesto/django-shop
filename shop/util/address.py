689ac2bc5f2f5073ede9a23e95b66c6ea8562351
Chris Glass <christopher.glass@divio.ch>
2011-05-23 11:43:15 +0200

    Implemented the helper functions in shop.util.address

--- shop/util/address.py
+++ shop/util/address.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
 from shop.util.loader import load_class
 
 """
@@ -16,14 +17,36 @@ def get_shipping_address_from_request(request):
     Get the shipping address from the request. This abstracts the fact that users
     can be either registered (and thus, logged in), or only session-based guests
     """
-    # TODO: Implement
+    if request.user and not isinstance(request.user, AnonymousUser):
+        # There is a logged-in user here.
+        shipping_address = request.user.shipping_address
+    else:
+        # The client is a guest - let's use the session instead.
+        session = getattr(request, 'session', None)
+        if session != None :
+            # There is a session
+            shipping_address_id = session.get('shipping_address_id')
+            if shipping_address_id:
+                shipping_address = AddressModel.objects.get(pk=shipping_address_id)
+    return shipping_address
 
 def get_billing_address_from_request(request):
     """
     Get the billing address from the request. This abstracts the fact that users
     can be either registered (and thus, logged in), or only session-based guests
     """
-    # TODO: Implement
+    if request.user and not isinstance(request.user, AnonymousUser):
+        # There is a logged-in user here.
+        billing_address = request.user.billing_address
+    else:
+        # The client is a guest - let's use the session instead.
+        session = getattr(request, 'session', None)
+        if session != None :
+            # There is a session
+            billing_address_id = session.get('billing_address_id')
+            if billing_address_id:
+                billing_address = AddressModel.objects.get(pk=billing_address_id)
+    return billing_address
     
 def assign_address_to_request(request, address, shipping=True):
     """
@@ -34,4 +57,18 @@ def assign_address_to_request(request, address, shipping=True):
     The `shipping` parameter controls whether the address is a shipping address 
     (default) or a billing address.
     """
-    # TODO: Implement
+    if request.user and not isinstance(request.user, AnonymousUser):
+        # There is a logged-in user here.
+        if shipping:
+            address.user_shipping = request.user
+            address.save()
+        else:
+            address.user_billing = request.user
+            address.save()
+    else:
+        # The client is a guest - let's use the session instead.
+        # There has to be a session. Otherwise it's fine to get an AttributeError
+        if shipping:
+            request.session['shipping_address_id'] = address.pk
+        else:
+            request.session['billing_address_id'] = address.pk
