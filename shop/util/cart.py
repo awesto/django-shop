b24f0f9875d54c7704b7e804f952b34aac36b4c3
Chris Glass <christopher.glass@divio.ch>
2011-07-13 13:13:52 +0200

    Re-added caching in the get_cart_from_request function

--- shop/util/cart.py
+++ shop/util/cart.py
@@ -9,25 +9,27 @@ def get_or_create_cart(request):
     exists), or to the session.
     """
     cart = None
-    if request.user and not isinstance(request.user, AnonymousUser):
-        # There is a logged in user
-        cart = Cart.objects.filter(user=request.user) # a list
-        if not cart: # if list is empty
-            cart = Cart.objects.create(user=request.user)
+    if not hasattr(request, '_cart'):
+        if request.user and not isinstance(request.user, AnonymousUser):
+            # There is a logged in user
+            cart = Cart.objects.filter(user=request.user) # a list
+            if not cart: # if list is empty
+                cart = Cart.objects.create(user=request.user)
+            else:
+                cart = cart[0] # Get the first one from the list
         else:
-            cart = cart[0] # Get the first one from the list
-    else:
-        session = getattr(request, 'session', None)
-        if session != None :
-            # There is a session
-            cart_id = session.get('cart_id')
-            if cart_id:
-                try:
-                    cart = Cart.objects.get(pk=cart_id)
-                except Cart.DoesNotExist:
-                    cart = None
-            if not cart:
-                cart = Cart.objects.create()
-                session['cart_id'] = cart.id
-        
+            session = getattr(request, 'session', None)
+            if session != None :
+                # There is a session
+                cart_id = session.get('cart_id')
+                if cart_id:
+                    try:
+                        cart = Cart.objects.get(pk=cart_id)
+                    except Cart.DoesNotExist:
+                        cart = None
+                if not cart:
+                    cart = Cart.objects.create()
+                    session['cart_id'] = cart.id
+        setattr(request, '_cart', cart)
+    cart = getattr(request, '_cart') # There we *must* have a cart
     return cart
