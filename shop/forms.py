85eda483cefe84cf94c6e59c3c4aec8ac3f630a1
Martin Brochhaus <mbrochh@gmail.com>
2011-11-17 02:23:47 +0800

    Using a dynamically generated form for cart view in order to validate input.

--- shop/forms.py
+++ shop/forms.py
@@ -17,3 +17,27 @@ class BillingShippingForm(forms.Form):
     """
     shipping_method = forms.ChoiceField(choices=get_shipping_backends_choices())
     payment_method = forms.ChoiceField(choices=get_billing_backends_choices())
+
+
+class CartDetailsForm(forms.Form):
+    """A dynamically generated form displaying all items in the cart."""
+
+    field_prefix = 'update_item'
+
+    def __init__(self, cart, *args, **kwargs):
+        """Instanciates the form and creates fields for each cart item."""
+        super(CartDetailsForm, self).__init__(*args, **kwargs)
+        self.cart = cart
+        for item in cart.get_updated_cart_items():
+            field_name = '%s_%d' % (self.field_prefix, item.id)
+            field = forms.IntegerField(label=item.product.name, min_value=0,
+                    initial=item.quantity)
+            field.cart_item = item
+            self.fields[field_name] = field
+
+    def save(self):
+        """Updates the quantities of the cart items."""
+        cleaned_data = self.cleaned_data
+        for key in cleaned_data:
+            cart_item_id = int(key[len(self.field_prefix)+1:])
+            self.cart.update_quantity(cart_item_id, cleaned_data[key])
