f28b206e3b0ef8fc4db2c9bf06a58ff304126b88
Jacob Rief <jacob.rief@gmail.com>
2011-12-18 22:58:33 +0100

    removed useles urls

--- shop/urls/__init__.py
+++ shop/urls/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, include
 from shop.views import ShopTemplateView
 
 
