6dbe9e7c06157b7318af0c8f72595f1721507ff6
Jacob Rief <jacob.rief@gmail.com>
2011-12-21 12:00:05 +0100

    OrderItem can be copied back to the Cart

--- shop/util/order.py
+++ shop/util/order.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 from django.contrib.auth.models import AnonymousUser
-from shop.models.ordermodel import Order
+from shop.models.ordermodel import Order, OrderItem
+from shop.util.cart import get_or_create_cart
 
 
 def get_order_from_request(request):
@@ -43,3 +44,15 @@ def add_order_to_request(request, order):
         # Add the order_id to the session There has to be a session. Otherwise
         # it's fine to get an AttributeError
         request.session['order_id'] = order.id
+
+def copy_order_item_to_cart(request, order, item_id):
+    """
+    Copy the item with the given id back to the cart.
+    """
+    item = OrderItem.objects.filter(order=order, id=item_id)
+    if item != None:
+        item = item[0]
+        cart = get_or_create_cart(request)
+        cart.save()
+        cart.add_product(item.product, item.quantity, item.variation)
+        cart.update()
