b28e37f82ea1ff51acad1d9c33df9d39c72c9922
Jacob Rief <jacob.rief@gmail.com>
2012-02-27 10:55:00 +0100

    If this class is mixed into a class derived from, say admin.ModelAdmin or admin.TabularInline all DecimalField's are localized.

--- shop/admin/mixins.py
+++ shop/admin/mixins.py
@@ -4,12 +4,18 @@ from django import forms
 
 class LocalizeDecimalFieldsMixin(object):
     '''
-    To be used as a mixin for localizing DecimalField's in the admin interface. 
+    To be used as a mixin for classes derived from admin.ModelAdmin,
+    admin.TabularInline, etc. which localizes the input fields for models of
+    with type DecimalField in the admin interface.
     '''
-    def __new__(cls, *args, **kwargs):
-        new_class = super(LocalizeDecimalFieldsMixin, cls).__new__(cls)
-        for field in new_class.base_fields.values():
-            if isinstance(field, forms.DecimalField):
-                field.localize = True
-                field.widget.is_localized = True
-        return new_class
+    class LocalizeDecimalFieldsForm(forms.ModelForm):
+        def __new__(cls, *args, **kwargs):
+            new_class = super(LocalizeDecimalFieldsMixin.LocalizeDecimalFieldsForm, cls).__new__(cls)
+            if hasattr(new_class, 'base_fields'):
+                for field in new_class.base_fields.values():
+                    if isinstance(field, forms.DecimalField):
+                        field.localize = True
+                        field.widget.is_localized = True
+            return new_class
+
+    form = LocalizeDecimalFieldsForm
