b5829849a78934e256023c06a96e5fc0a816d66a
Jacob Rief <jacob.rief@gmail.com>
2013-04-29 18:27:05 +0200

    Added safty check for callability of as_text() method

--- shop/models_bases/__init__.py
+++ shop/models_bases/__init__.py
@@ -410,7 +410,7 @@ class BaseOrder(models.Model):
         e.g. you can copy address instance and save FK to it in your order
         class.
         """
-        if  hasattr(billing_address, 'as_text'):
+        if hasattr(billing_address, 'as_text') and callable(billing_address.as_text):
             self.billing_address_text = billing_address.as_text()
             self.save()
 
@@ -422,7 +422,7 @@ class BaseOrder(models.Model):
         e.g. you can copy address instance and save FK to it in your order
         class.
         """
-        if hasattr(shipping_address, 'as_text'):
+        if hasattr(shipping_address, 'as_text') and callable(shipping_address.as_text):
             self.shipping_address_text = shipping_address.as_text()
             self.save()
 
