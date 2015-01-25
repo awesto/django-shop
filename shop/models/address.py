b555023726f28d25cc2e7d7acbefb50d48286b79
Jacob Rief <jacob.rief@gmail.com>
2015-01-25 17:58:28 +0100

    Added a basic abstract Address model to be used as a base for addresses

--- shop/models/address.py
+++ shop/models/address.py
@@ -2,70 +2,26 @@
 """
 Holds all the information relevant to the client (addresses for instance)
 """
+from six import with_metaclass
 from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from . import deferred
 
-from django.conf import settings
 
-BASE_ADDRESS_TEMPLATE = \
-_("""
-Name: %(name)s,
-Address: %(address)s,
-Zip-Code: %(zipcode)s,
-City: %(city)s,
-State: %(state)s,
-Country: %(country)s
-""")
-
-ADDRESS_TEMPLATE = getattr(settings, 'SHOP_ADDRESS_TEMPLATE',
-                           BASE_ADDRESS_TEMPLATE)
-USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
-
-class Country(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __unicode__(self):
-        return u'%s' % self.name
-
+class BaseAddress(with_metaclass(deferred.ForeignKeyBuilder, models.Model)):
     class Meta(object):
-        verbose_name = _('Country')
-        verbose_name_plural = _('Countries')
-
-
-class Address(models.Model):
-    user_shipping = models.OneToOneField(USER_MODEL, related_name='shipping_address',
-                                         blank=True, null=True)
-    user_billing = models.OneToOneField(USER_MODEL, related_name='billing_address',
-                                        blank=True, null=True)
-
-    name = models.CharField(_('Name'), max_length=255)
-    address = models.CharField(_('Address'), max_length=255)
-    address2 = models.CharField(_('Address2'), max_length=255, blank=True)
-    zip_code = models.CharField(_('Zip Code'), max_length=20)
-    city = models.CharField(_('City'), max_length=20)
-    state = models.CharField(_('State'), max_length=255)
-    country = models.ForeignKey(Country, verbose_name=_('Country'), blank=True,
-                                null=True)
-
-    class Meta(object):
-        verbose_name = _('Address')
+        abstract = True
+        verbose_name = _("Address")
         verbose_name_plural = _("Addresses")
+        ordering = ('priority_delivery', 'priority_invoice')
 
-    def __unicode__(self):
-        return '%s (%s, %s)' % (self.name, self.zip_code, self.city)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    priority_delivery = models.SmallIntegerField(default=0,
+        help_text=_("Priority of using this address for delivery"))
+    priority_invoice = models.SmallIntegerField(default=0,
+        help_text=_("Priority of using this address for invoicing"))
 
-    def clone(self):
-        new_kwargs = dict([(fld.name, getattr(self, fld.name))
-                           for fld in self._meta.fields if fld.name != 'id'])
-        return self.__class__.objects.create(**new_kwargs)
-
-    def as_text(self):
-        return ADDRESS_TEMPLATE % {
-            'name': self.name,
-            'address': '%s\n%s' % (self.address, self.address2),
-            'zipcode': self.zip_code,
-            'city': self.city,
-            'state': self.state,
-            'country': self.country,
-        }
+    def save(self, *args, **kwargs):
+        return super(BaseAddress).save(*args, **kwargs)
+        # max_priority = self.model.objects.filter().aggregate(max=Max(self.priority_delivery))['max'] or 0
