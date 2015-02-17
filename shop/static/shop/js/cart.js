057c34b278ad054d47497affb4c9216e73c346dc
Jacob Rief <jacob.rief@gmail.com>
2015-02-18 00:38:30 +0100

    can control items

--- shop/static/shop/js/cart.js
+++ shop/static/shop/js/cart.js
@@ -1,13 +1,11 @@
 (function(angular, undefined) {
 'use strict';
 
-// module: django.shop
+// module: django.shop, TODO: move this into a summary JS file
 var djangoShopModule = angular.module('django.shop', []);
 
 djangoShopModule.controller('CartController', ['$scope', '$http', 'shopConfig', function($scope, $http, shopConfig) {
-	var self = this;
-
-	self.loadCart = function() {
+	this.loadCart = function() {
 		$http.get(shopConfig.cartListURL).success(function(cart) {
 			console.log('loaded cart: ');
 			console.log(cart);
@@ -16,13 +14,36 @@ djangoShopModule.controller('CartController', ['$scope', '$http', 'shopConfig', 
 			console.error('Unable to fetch shopping cart: ' + msg);
 		});
 	}
+
+	function postCartItem(cart_item, method) {
+		console.log(cart_item);
+		var config = {headers: {'X-HTTP-Method-Override': method}};
+		$http.post(cart_item.url, cart_item, config).then(function(response) {
+			console.log(response);
+			return $http.get(shopConfig.cartListURL/* + '?digest'*/);
+		}).then(function(response) {
+			console.log(response);
+			angular.copy(response.data, $scope.cart);
+		}, function(error) {
+			console.error(error);
+		});
+	}
+
+	$scope.updateCartItem = function(cart_item) {
+		postCartItem(cart_item, 'PUT');
+	}
+
+	$scope.deleteCartItem = function(cart_item) {
+		postCartItem(cart_item, 'DELETE');
+	}
 }]);
 
-// This directive handles a djangoSHOP's cart
-djangoShopModule.directive('shopCart', function() {
+// Directive <shop-cart>
+// handle a djangoSHOP's cart
+djangoShopModule.directive('shopCart', function(shopConfig) {
 	return {
 		restrict: 'EA',
-		templateUrl: 'template/shop/cart.html',
+		templateUrl: 'shop/cart.html',
 		controller: 'CartController',
 		link: function(scope, element, attrs, cartCtrl) {
 			cartCtrl.loadCart();
@@ -30,11 +51,19 @@ djangoShopModule.directive('shopCart', function() {
 	};
 });
 
-djangoShopModule.directive('shopCartItem', function() {
+// Directive <shop-cart-item>
+// handle a djangoSHOP's cart item
+djangoShopModule.directive('shopCartItem', function(shopConfig) {
 	return {
 		require: '^shopCart',
 		restrict: 'EA',
-		templateUrl: 'template/shop/cart-item.html'
+		templateUrl: 'shop/cart-item.html',
+		controller: 'CartController'
+		/*
+		scope: {
+			cart_item: '=cart_item'
+		}
+		*/
 	};
 });
 
