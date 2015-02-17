4a9dc043f82f33edb4bf5115a8ccfabb5a44e2de
Jacob Rief <jacob.rief@gmail.com>
2015-02-17 18:15:33 +0100

    useless field

--- shop/views/cart.py
+++ shop/views/cart.py
@@ -16,7 +16,6 @@ class CartItemSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(lookup_field='pk', view_name='shop-api:cart-detail')
     line_subtotal = serializers.CharField(read_only=True)
     line_total = serializers.CharField(read_only=True)
-    current_total = serializers.CharField(read_only=True)
     details = BaseProductSerializer(source='product', read_only=True)
 
     class Meta:
