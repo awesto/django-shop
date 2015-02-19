8578c38a0f896df9c44ccedccd560a8bd363a2e4
Jacob Rief <jacob.rief@gmail.com>
2015-02-19 18:49:13 +0100

    shop settings file

---
+++ shop/settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings
+
+
+DEFAULT_CURRENCY = getattr(settings, 'SHOP_DEFAULT_CURRENCY', 'EUR')
+
+MONEY_FORMAT = getattr(settings, 'SHOP_MONEY_FORMAT', '{symbol} {amount}')
+
+DECIMAL_PLACES = getattr(settings, 'SHOP_DECIMAL_PLACES', 2)
+"""
+Number of decimal places for the internal representation of a price. Not visible to the customer.
+"""
