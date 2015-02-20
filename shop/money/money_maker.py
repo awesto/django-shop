c6bf2d25068d8a01aaa4af334e1025b177f6ddee
Jacob Rief <jacob.rief@gmail.com>
2015-02-20 10:00:54 +0100

    Instead of inheriting from Decimal, Money type embedds a Decimal object

--- shop/money/money_maker.py
+++ shop/money/money_maker.py
@@ -22,9 +22,9 @@ class MoneyMaker(type):
         Renders the price localized and formatted in its current currency.
         """
         try:
-            amount = number_format(Decimal(self).quantize(self._cents))
+            amount = number_format(self._dec.quantize(self._cents))
         except InvalidOperation:
-            raise ValueError("Can not represent {0} as Money type.".format(self.__repr__()))
+            raise ValueError("Can not represent {} as Money type.".format(self.__repr__()))
         context = dict(code=self._currency_code, symbol=self._currency[2],
                        currency=self._currency[3], amount=amount)
         return force_text(getattr(shop_settings, 'MONEY_FORMAT').format(**context))
@@ -33,42 +33,36 @@ class MoneyMaker(type):
         return self.__unicode__().encode('utf-8')
 
     def __repr__(self):
-        value = Decimal.__str__(self)
+        value = self._dec.__str__()
         return "{}('{}')".format(self.__class__.__name__, value)
 
     def __add__(self, other, context=None):
         self._assert_addable(other)
-        amount = Decimal.__add__(self, other, context)
+        amount = self._dec.__add__(other._dec, context)
         return self.__class__(amount)
 
     def __radd__(self, other, context=None):
-        self._assert_addable(other)
-        amount = Decimal.__add__(self, other, context)
-        return self.__class__(amount)
+        return self.__add__(other, context)
 
     def __sub__(self, other, context=None):
         self._assert_addable(other)
-        amount = Decimal.__sub__(self, other, context)
+        amount = self._dec.__sub__(other._dec, context)
         return self.__class__(amount)
 
     def __rsub__(self, other, context=None):
-        other._assert_addable(self)
-        amount = Decimal.__sub__(other, self, context)
-        return self.__class__(amount)
+        raise ValueError("Can not substract money from something else.")
 
     def __mul__(self, other, context=None):
         self._assert_multipliable(other)
-        amount = Decimal.__mul__(self, other, context)
+        amount = self._dec.__mul__(other, context)
         return self.__class__(amount)
 
     def __rmul__(self, other, context=None):
-        self._assert_multipliable(other)
-        amount = Decimal.__mul__(self, other, context)
-        return self.__class__(amount)
+        return self.__mul__(other, context)
 
     def __div__(self, other, context=None):
         self._assert_dividable(other)
-        amount = Decimal.__div__(self, other, context)
+        amount = self._dec.__div__(other, context)
         return self.__class__(amount)
 
     def __rdiv__(self, other, context=None):
@@ -76,13 +70,11 @@ class MoneyMaker(type):
 
     def __truediv__(self, other, context=None):
         self._assert_dividable(other)
-        amount = Decimal.__truediv__(self, other, context)
+        amount = self._dec.__truediv__(other, context)
         return self.__class__(amount)
 
     def __rtruediv__(self, other, context=None):
-        self._assert_dividable(other)
-        amount = Decimal.__truediv__(self, other, context)
-        return self.__class__(amount)
+        raise ValueError("Can not divide through a currency.")
 
     def __pow__(self, other, context=None):
         raise ValueError("Can not raise currencies to their power.")
@@ -103,15 +95,27 @@ class MoneyMaker(type):
         """
         Build a class type, which behaves similar to Decimal, but knows about its currency.
         """
+        def my_new(cls, value='0', context=None):
+            self = object.__new__(cls)
+            if isinstance(value, Decimal):
+                self._dec = value
+            else:
+                try:
+                    self._dec = Decimal(value, context)
+                except Exception as err:
+                    raise ValueError(err)
+            return self
+
         if currency_code is None:
             currency_code = shop_settings.DEFAULT_CURRENCY
         if currency_code not in CURRENCIES:
             raise ValueError("'{}' is an unknown currency code. Please check shop/money/iso4217.py".format(currency_code))
         name = str('MoneyIn' + currency_code)
         # the Promise is required so that the REST JSONEncoder serializes our money type
-        bases = (Promise, Decimal)
+        bases = (Promise,)
         cents = Decimal('.' + CURRENCIES[currency_code][1] * '0')
-        attrs = dict((k, v) for k, v in cls.__dict__.items() if k != '__new__')
-        attrs.update(_currency_code=currency_code, _currency=CURRENCIES[currency_code], _cents=cents)
+        attrs = dict((k, v) for k, v in cls.__dict__.items())
+        attrs.update(_currency_code=currency_code, _currency=CURRENCIES[currency_code],
+                     _cents=cents, __new__=my_new)
         new_class = type(name, bases, attrs)
         return new_class
