b737cb2b0a780ecdca050d5a0b763bc2669e58a6
Jacob Rief <jacob.rief@gmail.com>
2015-02-24 21:59:48 +0100

    stabilized primary URL layout

--- example/urls.py
+++ example/urls.py
@@ -1,13 +1,21 @@
-from django.conf.urls import patterns, include, url
-from example.myshop.views import MyOrderConfirmView
-
-from shop import urls as shop_urls
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, patterns, include
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from cms.sitemaps import CMSSitemap
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-
-    url(r'^checkout/confirm/$', MyOrderConfirmView.as_view(), name='checkout_shipping'),
-    (r'^', include(shop_urls)), # <-- That's the important bit
-)
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
+        {'sitemaps': {'cmspages': CMSSitemap}}, name='sitemap'),
+    url(r'^shop-api/', include('shop.urls.api', namespace='shop-api')),
+) + i18n_patterns('',
+    url(r'^admin/select2/', include('django_select2.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^', include('cms.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
