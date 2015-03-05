adb085007b178c741f574a9faf8fb9acb3b6bd93
Jacob Rief <jacob.rief@gmail.com>
2015-03-05 09:15:15 +0100

    dead code

--- shop/routes/cart.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from shop.views.cart import CartViewSet, WatchViewSet
-from . import shop_router
-
-shop_router.register(r'cart', CartViewSet, base_name='cart')
-shop_router.register(r'watch', WatchViewSet, base_name='watch')
