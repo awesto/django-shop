726c1442c2743fc124781014b6c2d9bdc1f87406
Jacob Rief <jacob.rief@gmail.com>
2015-03-19 16:08:14 +0100

    Renamed cart/main.html -> cart/cart.html

--- shop/cascade/cart.py
+++ shop/cascade/cart.py
@@ -20,8 +20,8 @@ class ShopCartPlugin(CascadePluginBase):
     @property
     def render_template(self):
         template_names = [
-            '{}/cart/main.html'.format(settings.SHOP_APP_LABEL),
-            'shop/cart/main.html',
+            '{}/cart/cart.html'.format(settings.SHOP_APP_LABEL),
+            'shop/cart/cart.html',
         ]
         return select_template(template_names)
 
