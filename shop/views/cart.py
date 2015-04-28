7aabd91768470b0e296f8f1cf9cb3b4d1bbac42c
Jacob Rief <jacob.rief@gmail.com>
2015-04-28 17:35:41 +0200

    Only process data from existings forms

--- shop/views/cart.py
+++ shop/views/cart.py
@@ -61,20 +61,24 @@ class CheckoutViewSet(BaseViewSet):
             if issubclass(p, DialogFormPluginBase):
                 self.dialog_forms.append(import_by_path(p.form_class))
 
-    @list_route(methods=['post'], url_path='update')
-    def update(self, request):
+    @list_route(methods=['post'], url_path='upload')
+    def upload(self, request):
         """
         All forms using the AngularJS directive `shop-dialog-form` have an implicit scope containing
-        an `update()` function. This function then may be connected to any input element, say
-        `ng-change="update()"`. If such an event triggers, the scope data is send to this
-        method using an Ajax POST request. This `update()` method then dispatches the form data
+        an `upload()` function. This function then may be connected to any input element, say
+        `ng-change="upload()"`. If such an event triggers, the scope data is send to this
+        method using an Ajax POST request. This `upload()` method then dispatches the form data
         to all forms registered through a `DialogFormPluginBase`.
         Afterwards the cart is updated, so that all cart modifiers run and adopt those changes.
         """
         cart = self.get_queryset()
 
         # sort posted form data by plugin order
-        dialog_data = [(fc, request.data.get(fc.scope_prefix.split('.', 1)[1], {})) for fc in self.dialog_forms]
+        dialog_data = []
+        for fc in self.dialog_forms:
+            key = fc.scope_prefix.split('.', 1)[1]
+            if key in request.data:
+                dialog_data.append((fc, request.data[key]))
         dialog_data = sorted(dialog_data, key=lambda tpl: int(tpl[1]['plugin_order']))
 
         # collect potential errors
