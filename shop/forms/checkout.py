7672dd324b52f0d0cd5aa12e23b1be165db18adc
Jacob Rief <jacob.rief@gmail.com>
2015-04-28 17:32:01 +0200

    Optionally use shipping address

--- shop/forms/checkout.py
+++ shop/forms/checkout.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.db.models import Max
 from django.contrib.auth import get_user_model
-from django.forms import fields
-from django.forms import widgets
+from django.forms import fields, widgets
 from django.utils.translation import ugettext_lazy as _
 from djangular.styling.bootstrap3.forms import Bootstrap3ModelForm
 from djangular.styling.bootstrap3.widgets import RadioSelect, RadioFieldRenderer, CheckboxInput
@@ -63,7 +63,7 @@ class AddressForm(DialogModelForm):
         priority = data and data.get('priority') or 0
         user = get_customer(request)
         filter_args = {'user': user, cls.priority_field: priority}
-        instance = cls.Meta.model.objects.filter(**filter_args).first()
+        instance = cls.get_model().objects.filter(**filter_args).first()
         address_form = cls(data=data, instance=instance)
         if address_form.is_valid():
             if not instance:
@@ -76,6 +76,27 @@ class AddressForm(DialogModelForm):
         else:
             return {address_form.form_name: dict(address_form.errors)}
 
+    @classmethod
+    def get_max_priority(cls, user):
+        """
+        Return the maximum priority for this address model.
+        """
+        aggr = cls.get_model().objects.filter(user=user).aggregate(Max(cls.priority_field))
+        return aggr.get('{}__max'.format(cls.priority_field, 0))
+
+    @classmethod
+    def set_address(cls, cart, instance):
+        # TODO: method must be connected to allow different priorities
+        address_form = cls(instance=instance)
+        data = address_form.initial
+        data.pop('id', None)
+        data.initial.pop('priority', None)
+        data.update({'user': cart.user, '{}__isnull'.format(cls.priority_field): False})
+        instance, created = cls.get_model().objects.get_or_create(**data)
+        if created:
+            instance.priority_invoice = cls.get_max_priority(cart.user) + 1
+            instance.save()
+
 
 class ShippingAddressForm(AddressForm):
     scope_prefix = 'data.shipping_address'
@@ -87,8 +108,9 @@ class ShippingAddressForm(AddressForm):
         }
 
     @classmethod
-    def set_address(cls, cart, address):
-        cart.shipping_address = address
+    def set_address(cls, cart, instance):
+        # TODO: super(ShippingAddressForm, cls).set_address(cart, instance)
+        cart.shipping_address = instance
 
 
 class InvoiceAddressForm(AddressForm):
@@ -110,13 +132,14 @@ class InvoiceAddressForm(AddressForm):
         `use_shipping_address` is active.
         """
         if data and data.pop('use_shipping_address', False):
-            scope_prefix = cls.scope_prefix.split('.', 1)[1]
-            data = request.data.get(scope_prefix)
-        return super(InvoiceAddressForm, cls).form_factory(request, data, cart)
+            cls.set_address(cart, cart.shipping_address)
+        else:
+            return super(InvoiceAddressForm, cls).form_factory(request, data, cart)
 
     @classmethod
-    def set_address(cls, cart, address):
-        cart.invoice_address = address
+    def set_address(cls, cart, instance):
+        # TODO: super(InvoiceAddressForm, cls).set_address(cart, instance)
+        cart.invoice_address = instance
 
 
 class PaymentMethodForm(DialogForm):
