514a5efab952463294f354f88e3b6477f69fb0d7
Jacob Rief <jacob.rief@gmail.com>
2015-05-02 23:42:33 +0200

    Added method to render address through template

--- shop/models/address.py
+++ shop/models/address.py
@@ -6,7 +6,9 @@ Holds all the information relevant to the client (addresses for instance)
 from six import with_metaclass
 from django.conf import settings
 from django.db import models
+from django.template.loader import select_template, Context
 from django.utils.translation import ugettext_lazy as _
+from shop import settings as shop_settings
 from . import deferred
 
 
@@ -21,5 +23,17 @@ class BaseAddress(with_metaclass(deferred.ForeignKeyBuilder, models.Model)):
         abstract = True
         verbose_name = _("Address")
         verbose_name_plural = _("Addresses")
+
+    def as_text(self):
+        """
+        Return the address as plain text to be used for printing, etc.
+        """
+        template_names = [
+            '{}/address.txt'.format(shop_settings.APP_LABEL),
+            'shop/address.txt',
+        ]
+        template = select_template(template_names)
+        context = Context({'address': self})
+        return template.render(context)
 
 AddressModel = deferred.MaterializedModel(BaseAddress)
