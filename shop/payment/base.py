17ec29a74e4c6ae75043b99a6f801234af270156
Jacob Rief <jacob.rief@gmail.com>
2015-05-02 23:44:03 +0200

    Added payment in andavce payment provider

---
+++ shop/payment/base.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import warnings
+from django.conf.urls import patterns
+from cms.models import Page
+
+
+class PaymentProvider(object):
+    """
+    Base class for all payment service providers.
+    """
+    @property
+    def namespace(self):
+        """
+        Use a unique namespace for this payment provider. It is used to build the communication URLs
+        exposed to an external payment service provider.
+        """
+        return NotImplemented
+
+    def get_urls(self):
+        """
+        Return a list of URL patterns for external communication with the payment service provider.
+        """
+        urlpatterns = patterns('')
+        return urlpatterns
+
+    def get_payment_request(self, cart, request):
+        """
+        Build a JavaScript expression which is evaluated by the success handler on the page
+        summiting the purchase command. When redirecting to another page, use:
+        ```
+        $window.location.href="URL-of-other-page";
+        ```
+        since this expression is evaluated inside an AngularJS directive.
+        """
+        return '$window.alert("Please implement method `get_payment_request` in the Python class inheriting from `PaymentProvider`!");'
+
+    def get_purchase_confirmation_url(self):
+        """
+        Return the URL of the CMS page confirming the purchase. This page normally says something
+        such as "Thank you for buying here".
+        """
+        try:
+            url = Page.objects.public().get(reverse_id='purchase-confirmation').get_absolute_url()
+        except Page.DoesNotExist:
+            warnings.warn("Please add a page with an id `purchase-confirmation` to the CMS.")
+            url = '/page_purchase-confirmation_not-found-in-cms'
+        return url
---
+++ shop/payment/prepayment.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from shop.models.order import OrderModel
+from .base import PaymentProvider
+
+
+class ForwardFundPayment(PaymentProvider):
+    """
+    Provides a simple prepayment payment provider.
+    """
+    namespace = 'forward-fund-payment'
+
+    def get_payment_request(self, cart, request):
+        order = OrderModel.objects.create_from_cart(cart, request)
+        return '$window.location.href="{}";'.format(self.get_purchase_confirmation_url())
