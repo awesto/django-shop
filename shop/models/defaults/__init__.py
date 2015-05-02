b224cd610c35972c30f0384917088b30e2614a2d
Jacob Rief <jacob.rief@gmail.com>
2015-05-02 23:45:06 +0200

    additional comments

---
+++ shop/models/defaults/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# The models in directory default have been added as a pure convenience and for demonstration
+# purpose. Whenever there is a need to use a modified version, copy one of these models into
+# the projects model directory and adopt it to your needs.
--- shop/modifiers/pool.py
+++ shop/modifiers/pool.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.core import exceptions
 from django.utils.importlib import import_module
 from shop import settings
