6dce78bc816849ebc29cb1e6fb4d2d1d0584afb5
Jacob Rief <jacob.rief@gmail.com>
2015-05-03 16:24:58 +0200

    Full support for order creation

--- shop/views/order.py
+++ shop/views/order.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from rest_framework import viewsets
-from shop.rest.serializers import OrderSerializer, OrderItemSerializer
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
+from shop.rest.serializers import OrderListSerializer, OrderDetailSerializer
 from shop.models.order import OrderModel
 
 
 class OrderViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = OrderSerializer
+    serializer_class = OrderListSerializer
 
     def get_queryset(self):
-        return OrderModel.objects.filter(user=self.request.user)
+        return OrderModel.objects.filter(user=self.request.user).order_by('-updated_at',)
 
-    def X_retrieve(self, request, *args, **kwargs):
-        order = super(OrderViewSet, self).retrieve(self, request, *args, **kwargs)
-        
-
-    def X_get_serializer(self, *args, **kwargs):
+    def get_serializer(self, *args, **kwargs):
         kwargs.update(context=self.get_serializer_context(), label='order')
-        many = kwargs.pop('many', False)
-        if many:
-            return OrderSerializer(*args, **kwargs)
-        return OrderItemSerializer(*args, **kwargs)
+        if kwargs.pop('many', False):
+            return OrderListSerializer(*args, **kwargs)
+        return OrderDetailSerializer(*args, **kwargs)
+
+    @list_route(url_path='last')
+    def last_order(self, request):
+        qs = self.get_queryset().last()
+        if qs:
+            serializer = OrderDetailSerializer(qs, context=self.get_serializer_context(), label='order')
+            return Response(serializer.data)
+        return Response(OrderDetailSerializer.errors, status=HTTP_404_NOT_FOUND)
