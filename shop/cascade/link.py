754c053268b4da3695d49c6ba1549de1094f6d7e
Jacob Rief <jacob.rief@gmail.com>
2015-05-05 09:03:18 +0200

    Fixed for unicode usage

--- shop/cascade/link.py
+++ shop/cascade/link.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.utils.translation import ugettext_lazy as _
 from django.db.models import get_model
 from django.core.exceptions import ObjectDoesNotExist
--- shop/urls/__init__.py
+++ shop/urls/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.conf.urls import include, url
 from . import rest_api
 from . import payment
