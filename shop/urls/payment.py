475a5be1d952d17e108991f35e09e1e30be84f23
Jacob Rief <jacob.rief@gmail.com>
2015-05-19 16:55:09 +0200

    Renamed payment_service -> payment_provider

--- shop/urls/payment.py
+++ shop/urls/payment.py
@@ -7,12 +7,12 @@ from shop.modifiers.pool import cart_modifiers_pool
 
 urlpatterns = patterns('')
 
-# For every payment modifier, load all the URLs the associate `payment_service` provides.
+# For every payment modifier, load the URLs from the associated `payment_provider`.
 for modifier in cart_modifiers_pool.get_payment_modifiers():
     try:
-        namespace = modifier.payment_service.namespace
+        namespace = modifier.payment_provider.namespace
         regexp = r'^{}/'.format(namespace)
-        urls = modifier.payment_service.get_urls()
+        urls = modifier.payment_provider.get_urls()
         pattern = patterns('',
             (regexp, include(urls, namespace=namespace))
         )
