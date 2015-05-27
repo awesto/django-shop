3297906a89c2da22788b8649916b77382b59ff23
Jacob Rief <jacob.rief@gmail.com>
2015-05-27 22:18:55 +0200

    Added date and time which can handle iso-8601 format

--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
 from django import template
+from django.conf import settings
+from django.utils import formats
 from django.template.loader import select_template
+from django.utils.dateformat import format, time_format
 from classytags.helpers import InclusionTag
-from classytags.core import Options
-from classytags.arguments import Argument
 from shop import settings as shop_settings
 from shop.models.cart import CartModel
 
@@ -28,3 +30,45 @@ class Cart(InclusionTag):
         context['cart'] = cart
         return context
 register.tag(Cart)
+
+
+@register.filter(expects_localtime=True, is_safe=False)
+def date(value, arg=None):
+    """
+    Alternative implementation to the built-in `date` template filter which also accepts the
+    date string in iso-8601 as passed in by the REST serializers.
+    """
+    if value in (None, ''):
+        return ''
+    if not isinstance(value, datetime):
+        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ")
+    if arg is None:
+        arg = settings.DATE_FORMAT
+    try:
+        return formats.date_format(value, arg)
+    except AttributeError:
+        try:
+            return format(value, arg)
+        except AttributeError:
+            return ''
+
+
+@register.filter(expects_localtime=True, is_safe=False)
+def time(value, arg=None):
+    """
+    Alternative implementation to the built-in `time` template filter which also accepts the
+    date string in iso-8601 as passed in by the REST serializers.
+    """
+    if value in (None, ''):
+        return ''
+    if not isinstance(value, datetime):
+        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ")
+    if arg is None:
+        arg = settings.TIME_FORMAT
+    try:
+        return formats.time_format(value, arg)
+    except AttributeError:
+        try:
+            return time_format(value, arg)
+        except AttributeError:
+            return ''
