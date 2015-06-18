dffebc2120a37a0427fece80d0b0fa6161537f27
Jacob Rief <jacob.rief@gmail.com>
2015-06-18 14:21:23 +0200

    With django-haystack-rst-1.4 no need to remove indices from serialized payload

--- shop/search/serializers.py
+++ shop/search/serializers.py
@@ -9,27 +9,21 @@ class ProductSearchSerializer(HaystackSerializer):
     The base serializer to represent one or more product fields for being returned as a
     result list during searches.
     """
-    text = serializers.SerializerMethodField()  # dummy fields to bypass serialization
-    autocomplete = serializers.SerializerMethodField()
     price = serializers.SerializerMethodField()
 
     class Meta:
         fields = ('text', 'autocomplete', 'name', 'product_url', 'price', 'body')
-
-    def get_text(self, obj):
-        pass
-
-    def get_autocomplete(self, obj):
-        pass
+        ignore_fields = ('text', 'autocomplete',)
 
     def get_price(self, search_result):
         """
-        The price can't be stored inside the index but must be fetched from the resolved model.
+        The price can't be stored inside the search index but must be fetched from the resolved
+        model. In case your product models have fixed prices, try to store and retrieve them
+        from the search index, because that's much faster.
         """
-        return search_result.object.get_price(self.context['request'])
+        if search_result.object:
+            return search_result.object.get_price(self.context['request'])
 
     def to_representation(self, instance):
         representation = super(ProductSearchSerializer, self).to_representation(instance)
-        representation.pop('text')
-        representation.pop('autocomplete')
         return representation
