375394282c2c10b3b3f1ede8f7b63a2c599df1e7
Jacob Rief <jacob.rief@gmail.com>
2015-06-30 15:51:44 +0200

    Removed private field

--- example/myshop/models/commodity.py
+++ example/myshop/models/commodity.py
@@ -9,9 +9,6 @@ from .shopmodels import Product
 
 @python_2_unicode_compatible
 class Commodity(Product):
-    cost = models.CharField(choices=(('per_piece', _("per piece")), ('per_meter', _("per meter")),),
-        max_length=12, default='per_piece', verbose_name='')
-
     class Meta:
         app_label = settings.SHOP_APP_LABEL
         verbose_name = _("Commodity")
@@ -19,8 +16,3 @@ class Commodity(Product):
 
     def __str__(self):
         return self.name
-
-    def is_in_cart(self, cart, extra):
-        if self.cost == 'per_meter':
-            return None
-        return super(Commodity, self).is_in_cart(cart, extra)
