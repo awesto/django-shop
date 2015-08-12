c18cf43b81cbdd3e3c7aada6cac9e97e0f7f47ae
Jacob Rief <jacob.rief@gmail.com>
2015-08-12 17:10:31 +0200

    added TODO

--- shop/urls/rest_api.py
+++ shop/urls/rest_api.py
@@ -5,7 +5,7 @@ from shop.views.cart import CartViewSet, WatchViewSet
 from shop.views.checkout import CheckoutViewSet
 from rest_framework import routers
 
-router = routers.DefaultRouter()  # trailing_slash=False
+router = routers.DefaultRouter()  # TODO: try with trailing_slash=False
 router.register(r'cart', CartViewSet, base_name='cart')
 router.register(r'watch', WatchViewSet, base_name='watch')
 router.register(r'checkout', CheckoutViewSet, base_name='checkout')
