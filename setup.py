c49ed4a18291bc071e2e91525fa13227dd297034
chris <chris@kavefish.net>
2015-09-05 20:32:31 -0400

    remove dependency on South; not required for Django 1.8+

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'Django>=1.4',
         'django-classy-tags>=0.3.3',
         'django-polymorphic>=0.2',
-        'south>=0.7.2',
         'jsonfield>=0.9.6'
     ],
     packages=find_packages(exclude=["example", "example.*"]),
