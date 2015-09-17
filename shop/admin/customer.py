54934a8a814fe47adfc56dd32209883b2de3769a
Jacob Rief <jacob.rief@gmail.com>
2015-09-17 20:35:50 +0200

    Override built-in UserAdmin for Customer

--- shop/admin/customer.py
+++ shop/admin/customer.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-from django.utils.html import format_html
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
+from shop.models.customer import CustomerModel
 
-from shop.models.customer import CustomerModel as Customer
 
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('identifier', 'creation_date',)
+class CustomerInlineAdmin(admin.StackedInline):
+    model = CustomerModel
 
-admin.site.register(Customer, CustomerAdmin)
 
+class CustomerChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = get_user_model()
+
+
+class CustomerAdmin(UserAdmin):
+    form = CustomerChangeForm
+    inlines = (CustomerInlineAdmin,)
+    #fieldsets = (
+    #    (None, {'fields': ('user__email', 'user__password')}),
+    #    (_('Personal info'), {'fields': ('salutation', 'user__first_name', 'user__last_name',)}),
+    #    #(_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+    #    #(_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    #)
