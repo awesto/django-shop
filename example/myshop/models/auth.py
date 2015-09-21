16e9cdc67a7b6e234ee963930d75730bbead4497
Jacob Rief <jacob.rief@gmail.com>
2015-09-21 21:32:36 +0200

    fixed model usage

--- example/myshop/models/auth.py
+++ example/myshop/models/auth.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.conf import settings
-from shop.models.auth import BaseCustomer
+from shop.models.customer import BaseCustomer
 
 
 class Customer(BaseCustomer):
