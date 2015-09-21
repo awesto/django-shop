6448f9947c10a47494d79dc3806eb684249e3378
Jacob Rief <jacob.rief@gmail.com>
2015-09-21 11:10:22 +0200

    Override emulated customer for Segmentation on RequestContext

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from shop.models.customer import CustomerModel
 
 
 def customer(request):
@@ -7,6 +8,11 @@ def customer(request):
     Add the customer to the RequestContext
     """
     assert hasattr(request, 'customer'), "The request object does not contain a customer. Edit your MIDDLEWARE_CLASSES setting to insert 'shop.middlerware.CustomerMiddleware'."
-    return {
-        'customer': request.customer,
-    }
+
+    context = {'customer': request.customer}
+    if request.user.is_staff:
+        try:
+            context.update(customer=CustomerModel.objects.get(pk=request.session['emulate_user_id']))
+        except (CustomerModel.DoesNotExist, KeyError):
+            pass
+    return context
