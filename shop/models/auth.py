f971f2a47499ef3ac1ffd9a2ad74ba380715cece
Jacob Rief <jacob.rief@gmail.com>
2015-09-22 12:51:06 +0200

    updated docs strings

--- shop/models/auth.py
+++ shop/models/auth.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 """
-Alternative implementation of Django's authentication model.
+Alternative implementation of Django's authentication User model, which allows to authenticate
+against the email field in addition to the username fields.
+This alternative implementation is activated by setting ``AUTH_USER_MODEL = 'shop.User'`` in
+settings.py, otherwise the default Django or another implementation is used.
 """
 import re
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
@@ -36,15 +39,11 @@ class UserManager(BaseUserManager):
 @python_2_unicode_compatible
 class User(AbstractBaseUser, PermissionsMixin):
     """
-    Replace the Django User model, so that the email address is used instead of the username.
-    To activate, import this User model into the models.py of your your application and in
-    settings.py, point ``AUTH_USER_MODEL`` onto this imported User model.
+    Alternative implementation of Django's User model allowing to authenticate
+    against the email field in addition to the username field.
     """
     USERNAME_REGEX = re.compile('^[\w.@+-]+$')
 
-    email = models.EmailField(_("Email address"), null=True, default=None, unique=True, max_length=254)
-
-    # some authentication require the username, regardless of the USERNAME_FIELD setting below
     username = models.CharField(_("Username"), max_length=30, unique=True,
         help_text=_("Required. 30 characters or fewer. Letters, numbers and @/./+/-/_ characters"),
         validators=[(validators.RegexValidator(USERNAME_REGEX), _("Enter a valid username."), 'invalid')])
@@ -52,6 +51,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     # copied from django.contrib.auth.models.AbstractUser
     first_name = models.CharField(_("First name"), max_length=30, blank=True)
     last_name = models.CharField(_("Last name"), max_length=30, blank=True)
+
+    # this is the only field, which differs from the default implementation
+    email = models.EmailField(_("Email address"), null=True, default=None, blank=True, unique=True, max_length=254)
     is_staff = models.BooleanField(_("staff status"), default=False,
         help_text=_("Designates whether the user can log into this admin site."))
     is_active = models.BooleanField(_("active"), default=True,
