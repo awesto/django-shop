c65b7cf744c91769e6d165195dd1f4d421fc52f9
Jacob Rief <jacob.rief@gmail.com>
2015-09-22 12:51:29 +0200

    Reuse default UserManager

--- shop/models/auth.py
+++ shop/models/auth.py
@@ -7,33 +7,13 @@ This alternative implementation is activated by setting ``AUTH_USER_MODEL = 'sho
 settings.py, otherwise the default Django or another implementation is used.
 """
 import re
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.core import validators
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
-
-
-class UserManager(BaseUserManager):
-
-    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
-        now = timezone.now()
-        if not email:
-            raise ValueError("The given email address must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, is_staff=is_staff, is_active=True,
-            is_superuser=is_superuser, last_login=now, date_joined=now, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        return self._create_user(email, password, False, False, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, True, True, **extra_fields)
 
 
 @python_2_unicode_compatible
