46fb0800d3bdd4da92824cb9914662160ceea14f
Jacob Rief <jacob.rief@gmail.com>
2015-10-01 08:09:55 +0200

    More meaningful template content, for abused ShopOrderViewPlugin

--- shop/cascade/order.py
+++ shop/cascade/order.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.template import Template
 from django.template.loader import select_template
 from django.utils.translation import ugettext_lazy as _
 from cms.plugin_pool import plugin_pool
@@ -17,19 +18,17 @@ class ShopOrderViewsPlugin(ShopPluginBase):
         many = context.get('many')
         if many is True:
             # render Order List View
-            template_names = [
+            return select_template([
                 '{}/order/list.html'.format(shop_settings.APP_LABEL),
                 'shop/order/list.html',
-            ]
-        elif many is False:
+            ])
+        if many is False:
             # render Order Detail View
-            template_names = [
+            return select_template([
                 '{}/order/detail.html'.format(shop_settings.APP_LABEL),
                 'shop/order/detail.html',
-            ]
-        else:
-            # can happen, if this plugin is abused outside of an OrderView
-            template_names = ['cascade/generic/naked.html']
-        return select_template(template_names)
+            ])
+        # can happen, if this plugin is abused outside of an OrderView
+        return Template('<!-- Order Views Plugin used outside of a page with a CMS app “View Order” -->')
 
 plugin_pool.register_plugin(ShopOrderViewsPlugin)
