542de7a28c330d775f1011e90b4d3a9f184db8bc
Jacob Rief <jacob.rief@gmail.com>
2015-10-08 09:24:48 +0200

    fixed: manage createsuperuser works now

--- email_auth/models.py
+++ email_auth/models.py
@@ -44,6 +44,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     class Meta:
         db_table = 'auth_user'
