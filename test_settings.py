c7541132e030571a94ba4c6644c8fbc99cc1e987
Jacob Rief <jacob.rief@gmail.com>
2015-10-26 23:54:25 +0100

    updated unit test settings

--- test_settings.py
+++ test_settings.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 SECRET_KEY = 'test'
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     'django.contrib.auth',
+    'email_auth',
     'django.contrib.sites',
     'django.contrib.contenttypes',
     'treebeard',
@@ -9,11 +13,20 @@ INSTALLED_APPS = [
     'post_office',
     'filer',
     'shop',
-]
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'shop.middleware.CustomerMiddleware',
+)
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
 )
+
+SILENCED_SYSTEM_CHECKS = ['auth.W004']
 
 SHOP_APP_LABEL = 'testshop'
 
@@ -25,3 +38,9 @@ DATABASES = {
 }
 
 SERIALIZATION_MODULES = {'shop': 'shop.money.serializers'}
+
+AUTH_USER_MODEL = 'email_auth.User'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,10 +1,51 @@
-from django.test import TestCase
-
+# -*- coding: utf-8
+from __future__ import unicode_literals
+from django.contrib.auth import get_user_model
+from django.test import TestCase, RequestFactory
 import mock
-
 from rest_framework.test import APIRequestFactory
-
 from shop.rest.auth import PasswordResetSerializer
+from shop.models.defaults.customer import Customer
+
+
+class CustomerTest(TestCase):
+    customers = {
+        'lisa': {
+            # Lisa has a Django account, but no Customer account yet
+            'username': 'lisa',
+            'email': 'lisa@thesimpsons.com',
+            'password': 'asil',
+        },
+        'bart': {
+            # Bart has a Django account and a Customer account
+            #'username': 'bart',
+            'email': 'bart@thesimpsons.com',
+            #'password': 'trab',
+            'salutation': 'mr',
+        },
+        'maggie': {
+            # Maggie doesn't have an account yet, but will register during checkout
+            'username': 'maggie',
+            'email': 'maggie@thesimpsons.com',
+            'password': 'eiggam',
+            'salutation': 'n/a',
+        },
+        'homer': {
+            # Homer does not register and buys as a guest
+            'email': 'homer@thesimpsons.com',
+        },
+    }
+
+    def setUp(self):
+        super(CustomerTest, self).setUp()
+        User = get_user_model()
+        self.lisa = User.objects.create(**self.customers['lisa'])
+        #self.bart = Customer.objects.create(**self.customers['bart'])
+        self.factory = RequestFactory()
+
+    def test_bart(self):
+        lisa = get_user_model().objects.get(username=self.customers['lisa']['username'])
+        print lisa
 
 
 class PasswordResetSerializerTest(TestCase):
