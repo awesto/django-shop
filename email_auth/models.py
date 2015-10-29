79d67ca8d33b4dddb80a09f05818167e32362811
Jacob Rief <jacob.rief@gmail.com>
2015-10-29 21:04:23 +0100

    excluse self from unique validation

--- email_auth/models.py
+++ email_auth/models.py
@@ -78,7 +78,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def validate_unique(self, exclude=None):
         super(User, self).validate_unique(exclude)
-        if get_user_model().objects.filter(is_active=True, email__exact=self.email).exists():
+        if get_user_model().objects.exclude(pk=self.pk).filter(is_active=True, email__exact=self.email).exists():
             msg = _("A customer with the e-mail address ‘{email}’ already exists.\n"
                     "If you have used this address previously, try to reset the password.")
             raise ValidationError({'email': msg.format(email=self.email)})
