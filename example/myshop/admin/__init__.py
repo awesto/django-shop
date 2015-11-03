2eddc8699c9f177b55b4626b59623bb58bc525ac
Jacob Rief <jacob.rief@gmail.com>
2015-11-03 11:28:16 +0100

    register customer admin into shop

--- example/myshop/admin/__init__.py
+++ example/myshop/admin/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib import admin
+from shop.admin.customer import CustomerProxy, CustomerAdmin
 from . import product
-#from . import commodity
+
+admin.site.register(CustomerProxy, CustomerAdmin)
