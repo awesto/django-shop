16520f6339cacc8159f72ad4556575df245bf592
Jacob Rief <jacob.rief@gmail.com>
2015-11-24 17:03:23 +0100

    remove unused import

--- shop/views/product.py
+++ shop/views/product.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 import os
 from django.db.models import Q
-from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.utils.translation import get_language_from_request
 from rest_framework import generics
