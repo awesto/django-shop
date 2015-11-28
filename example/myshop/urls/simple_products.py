2fcc821470a77b766f8cd274a33416a220931386
Jacob Rief <jacob.rief@gmail.com>
2015-11-28 18:03:53 +0100

    distinguish between simple and polymorphic products

---
+++ example/myshop/urls/simple_products.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf.urls import patterns, url
+from rest_framework.settings import api_settings
+from shop.rest.filters import CMSPagesFilterBackend
+from shop.rest.serializers import AddToCartSerializer
+from shop.views.product import AddToCartView, ProductListView, ProductRetrieveView
+from shop.search.views import SearchView
+from myshop.serializers import ProductSummarySerializer, ProductDetailSerializer
+
+list_options = dict(
+    serializer_class=ProductSummarySerializer,
+    filter_backends=api_settings.DEFAULT_FILTER_BACKENDS + [CMSPagesFilterBackend()],
+)
+detail_options = dict(
+    serializer_class=ProductDetailSerializer,
+    lookup_field='slug',
+)
+add2cart_options = dict(
+    serializer_class=AddToCartSerializer,
+    lookup_field='slug',
+)
+
+urlpatterns = patterns('',
+    url(r'^$', ProductListView.as_view(**list_options)),
+    url(r'^(?P<slug>[\w-]+)$', ProductRetrieveView.as_view(**detail_options)),
+    url(r'^(?P<slug>[\w-]+)/add-to-cart', AddToCartView.as_view(**add2cart_options)),
+    url(r'^autocomplete/', SearchView.as_view()),
+)
