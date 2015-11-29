604a7a0a604efa3a6472c9b14a9f2e7ea28b57c2
Jacob Rief <jacob.rief@gmail.com>
2015-11-29 15:05:04 +0100

    simplified urlpatterns for usage in CMS apphook

--- example/myshop/urls/simple_products.py
+++ example/myshop/urls/simple_products.py
@@ -3,27 +3,18 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, url
 from rest_framework.settings import api_settings
 from shop.rest.filters import CMSPagesFilterBackend
-from shop.rest.serializers import AddToCartSerializer
 from shop.views.catalog import AddToCartView, ProductListView, ProductRetrieveView
 from shop.search.views import SearchView
 from myshop.serializers import ProductSummarySerializer, ProductDetailSerializer
 
-list_options = dict(
-    serializer_class=ProductSummarySerializer,
-    filter_backends=api_settings.DEFAULT_FILTER_BACKENDS + [CMSPagesFilterBackend()],
-)
-detail_options = dict(
-    serializer_class=ProductDetailSerializer,
-    lookup_field='slug',
-)
-add2cart_options = dict(
-    serializer_class=AddToCartSerializer,
-    lookup_field='slug',
-)
-
 urlpatterns = patterns('',
-    url(r'^$', ProductListView.as_view(**list_options)),
-    url(r'^(?P<slug>[\w-]+)$', ProductRetrieveView.as_view(**detail_options)),
-    url(r'^(?P<slug>[\w-]+)/add-to-cart', AddToCartView.as_view(**add2cart_options)),
+    url(r'^$', ProductListView.as_view(
+        serializer_class=ProductSummarySerializer,
+        filter_backends=api_settings.DEFAULT_FILTER_BACKENDS + [CMSPagesFilterBackend()],
+    )),
+    url(r'^(?P<slug>[\w-]+)$', ProductRetrieveView.as_view(
+        serializer_class=ProductDetailSerializer
+    )),
+    url(r'^(?P<slug>[\w-]+)/add-to-cart', AddToCartView.as_view()),
     url(r'^autocomplete/', SearchView.as_view()),
 )
