7daf5ab03a89dc1e6c29b0028a72932056eecdc3
Jacob Rief <jacob.rief@gmail.com>
2015-12-07 23:55:13 +0100

    Select smartphone model using select box

--- example/myshop/urls/polymorphic_products.py
+++ example/myshop/urls/polymorphic_products.py
@@ -7,7 +7,6 @@ from shop.views.catalog import AddToCartView, ProductListView, ProductRetrieveVi
 from shop.search.views import SearchView
 from myshop.serializers import (ProductSummarySerializer, ProductDetailSerializer,
     AddSmartphoneToCartSerializer)
-from myshop.models.polymorphic.smartphone import SmartPhone
 
 urlpatterns = patterns('',
     url(r'^$', ProductListView.as_view(
@@ -18,11 +17,8 @@ urlpatterns = patterns('',
         serializer_class=ProductDetailSerializer,
     )),
     url(r'^(?P<slug>[\w-]+)/add-to-cart', AddToCartView.as_view()),
-    url(r'^smartphone/(?P<product_code>\d+)/add-to-cart', AddToCartView.as_view(
-        product_model=SmartPhone,
+    url(r'^(?P<slug>[\w-]+)/add-smartphone-to-cart', AddToCartView.as_view(
         serializer_class=AddSmartphoneToCartSerializer,
-        lookup_field='product_code',
-        lookup_url_kwarg='product_code',
-    ), name='add-smartphone-to-cart'),
+    )),
     url(r'^autocomplete/', SearchView.as_view()),
 )
