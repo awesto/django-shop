fd9d6408c74bb32ad9efaa8b3b9752266702f59f
Jacob Rief <jacob.rief@gmail.com>
2016-01-20 18:31:17 +0100

    moved extra-field from defaults.CartItem to CartItemBase

--- shop/models/defaults/cart_item.py
+++ shop/models/defaults/cart_item.py
@@ -3,12 +3,9 @@ from __future__ import unicode_literals
 
 from django.core.validators import MinValueValidator
 from django.db.models import IntegerField
-from django.utils.translation import ugettext_lazy as _
-from jsonfield.fields import JSONField
 from shop.models import cart
 
 
 class CartItem(cart.BaseCartItem):
     """Default materialized model for CartItem"""
     quantity = IntegerField(validators=[MinValueValidator(0)])
-    extra = JSONField(default={}, verbose_name=_("Arbitrary information for this cart item"))
