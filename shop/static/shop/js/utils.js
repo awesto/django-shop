8d128fed34056c17654d800f1d8739591d3510e5
Jacob Rief <jacob.rief@gmail.com>
2016-02-03 10:57:01 +0100

    Removed translationProvider in favor of djangoShopProvider

---
+++ shop/static/shop/js/utils.js
@@ -0,0 +1,37 @@
+(function(angular, undefined) {
+'use strict';
+
+// module: django.shop, TODO: move this into a summary JS file
+var djangoShopModule = angular.module('django.shop.utils', []);
+
+// Directive <shop-time>
+// handle a djangoSHOP's timestamp
+djangoShopModule.directive('shopTimestamp', ['$filter', '$locale', function($filter, $locale) {
+	return {
+		restrict: 'EAC',
+		link: function(scope, element, attrs) {
+			var timestamp = new Date(attrs.shopTimestamp);
+			element.html($filter('date')(timestamp, attrs.timeFormat));
+		}
+	};
+}]);
+
+djangoShopModule.provider('djangoShop', function() {
+	var self = this;
+
+	this.setTranslations = function(translations) {
+		debugger;
+		self.translations = translations;
+	};
+
+	this.dialogController = 'DialogController';  // the default
+	this.setDialogController = function(controller) {
+		self.dialogController = controller;
+	};
+
+	this.$get = function() {
+		return self;
+	};
+});
+
+})(window.angular);
