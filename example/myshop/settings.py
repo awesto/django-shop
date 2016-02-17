6d294e1ab91731a314ca8594ad5a3e8fd9eddc61
Jacob Rief <jacob.rief@gmail.com>
2016-02-17 17:35:24 +0100

    Added generic commodity to polymorphic products

--- example/myshop/settings.py
+++ example/myshop/settings.py
@@ -388,25 +388,35 @@ CMS_CACHE_DURATIONS = {
 
 CMS_PERMISSION = False
 
+CACSCADE_WORKAREA_GLOSSARY = {
+    'breakpoints': ['xs', 'sm', 'md', 'lg'],
+    'container_max_widths': {'xs': 750, 'sm': 750, 'md': 970, 'lg': 1170},
+    'fluid': False,
+    'media_queries': {
+        'xs': ['(max-width: 768px)'],
+        'sm': ['(min-width: 768px)', '(max-width: 992px)'],
+        'md': ['(min-width: 992px)', '(max-width: 1200px)'],
+        'lg': ['(min-width: 1200px)'],
+    },
+}
+
 CMS_PLACEHOLDER_CONF = {
     'Breadcrumb': {
         'plugins': ['BreadcrumbPlugin'],
         #  'text_only_plugins': ['TextLinkPlugin'],
         #  'parent_classes': {'BootstrapRowPlugin': []},
         #  'require_parent': False,
-        'glossary': {
-            'breakpoints': ['xs', 'sm', 'md', 'lg'],
-            'container_max_widths': {'xs': 750, 'sm': 750, 'md': 970, 'lg': 1170},
-            'fluid': False,
-            'media_queries': {
-                'xs': ['(max-width: 768px)'],
-                'sm': ['(min-width: 768px)', '(max-width: 992px)'],
-                'md': ['(min-width: 992px)', '(max-width: 1200px)'],
-                'lg': ['(min-width: 1200px)'],
-            },
-        },
+        'glossary': CACSCADE_WORKAREA_GLOSSARY,
+    },
+    'Commodity Details': {
+        'plugins': ['BootstrapRowPlugin', 'TextPlugin', 'ImagePlugin', 'PicturePlugin'],
+        'text_only_plugins': ['TextLinkPlugin'],
+        'parent_classes': {'BootstrapRowPlugin': []},
+        'require_parent': False,
+        'glossary': CACSCADE_WORKAREA_GLOSSARY,
     },
 }
+
 
 CMSPLUGIN_CASCADE_PLUGINS = ('cmsplugin_cascade.segmentation', 'cmsplugin_cascade.generic',
     'cmsplugin_cascade.link', 'shop.cascade', 'cmsplugin_cascade.bootstrap3',)
