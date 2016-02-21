a4aa63fc33d7e54b6e14c91a222a4877f92bf881
Jacob Rief <jacob.rief@gmail.com>
2016-02-21 13:19:20 +0100

    used touched file for bootstrap

--- example/wsgi.py
+++ example/wsgi.py
@@ -1,22 +1,35 @@
+import os
 from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 try:
     import uwsgidecorators
-except ImportError:
-    uwsgidecorators = None
 
-application = get_wsgi_application()
-call_command('rebuild_index', interactive=False)
-
-if uwsgidecorators:
     @uwsgidecorators.timer(15)
     def send_queued_mail(num):
+        """
+        Send queued mail every 15 seconds
+        """
         call_command('send_queued_mail')
 
     @uwsgidecorators.cron(0, 6, -1, -1, -1)
     def rebuild_index(num):
+        """
+        Rebuild search index in the morning
+        """
         call_command('rebuild_index', interactive=False)
 
     @uwsgidecorators.cron(30, 5, -1, -1, 0)
     def shopcustomers(num):
+        """
+        Delete expired customers every Sunday
+        """
         call_command('shopcustomers', delete_expired=True)
+except ImportError:
+    pass
+
+application = get_wsgi_application()
+
+# rebuild index on first bootstrap
+if os.path.isfile('bootstrap'):
+    call_command('rebuild_index', interactive=False)
+    os.remove('bootstrap')
