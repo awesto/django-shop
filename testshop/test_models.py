7b84a54a1fc10ee3b4556a45c1f54fa4acedcaf0
Jacob Rief <jacob.rief@gmail.com>
2016-04-06 00:20:44 +0200

    updated unit tests

--- testshop/test_models.py
+++ testshop/test_models.py
@@ -1,8 +1,29 @@
+# -*- coding: utf-8
+from __future__ import unicode_literals
+
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from shop.models.defaults.address import ShippingAddress, BillingAddress  # noqa
+from shop.models.defaults.customer import Customer
 
 
 class AddressTest(TestCase):
+    def setUp(self):
+        super(AddressTest, self).setUp()
+        User = get_user_model()
+        user = {
+            'username': 'john',
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email': 'john@example.com',
+            'password': 'secret',
+        }
+        user = User.objects.create(**user)
+        self.customer = Customer.objects.create(user=user)
+        self.assertGreaterEqual(self.customer.pk, 1)
 
-    def test_can_import(self):
-        pass
-        #from shop.models.defaults.address import Address  # noqa
+    def test_shipping_address(self):
+        shipping_addr = ShippingAddress.objects.create(priority=1, customer=self.customer)
+        self.assertGreaterEqual(shipping_addr.id, 1)
+        billing_addr = BillingAddress.objects.create(priority=1, customer=self.customer)
+        self.assertGreaterEqual(shipping_addr.id, 1)
