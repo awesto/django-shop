55f15cc1398b8901f7ce480e1b3533a52720c137
Jacob Rief <jacob.rief@gmail.com>
2016-04-28 13:29:37 +0200

    Refactored example/properties.py -> manufacturer.py

--- example/myshop/admin/__init__.py
+++ example/myshop/admin/__init__.py
@@ -5,14 +5,20 @@ from django.conf import settings
 from django.contrib import admin
 from shop.admin.customer import CustomerProxy, CustomerAdmin
 from shop.models.order import OrderModel
-from . import properties
 
-if settings.SHOP_TUTORIAL == 'simple':
+
+# models defined by the myshop instance itself
+if settings.SHOP_TUTORIAL == 'commodity' or settings.SHOP_TUTORIAL == 'i18n_commodity':
+    from shop.admin.defaults import commodity
+elif settings.SHOP_TUTORIAL == 'simple':
+    from . import manufacturer
     from .simple import smartcard, order
 elif settings.SHOP_TUTORIAL == 'i18n':
+    from . import manufacturer
     from .i18n import smartcard
     from .simple import order
 elif settings.SHOP_TUTORIAL == 'polymorphic':
+    from . import manufacturer
     from .polymorphic import product, order
 
 admin.site.register(CustomerProxy, CustomerAdmin)
