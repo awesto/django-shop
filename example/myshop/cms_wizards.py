8516ab70b49a155b3086324123457f501080ee1a
Jacob Rief <jacob.rief@gmail.com>
2016-06-13 14:46:31 +0200

    Added wizard for adding a Commodity

---
+++ example/myshop/cms_wizards.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from cms.wizards.wizard_base import Wizard
+from cms.wizards.wizard_pool import wizard_pool
+from shop.models.defaults.commodity import Commodity
+from shop.forms.wizards import CommodityWizardForm
+
+
+if settings.SHOP_TUTORIAL in ('commodity', 'i18n_commodity'):
+    commodity_wizard = Wizard(
+        title=_("New Commodity"),
+        weight=200,
+        form=CommodityWizardForm,
+        description=_("Create a new Commodity instance"),
+        model=Commodity,
+        template_name='shop/wizards/create_product.html'
+    )
+    wizard_pool.register(commodity_wizard)
