34aace5de00c9a5d47fa3b16bf086ce2e77ef13f
Jacob Rief <jacob.rief@gmail.com>
2016-06-17 23:27:53 +0200

    demo for sharables on image and picture

--- example/myshop/settings.py
+++ example/myshop/settings.py
@@ -455,6 +455,11 @@ CMSPLUGIN_CASCADE = {
         'ShopProceedButton',
         'CarouselPlugin',
     ),
+    'plugins_with_sharables': {
+        'BootstrapImagePlugin': ('image-shapes', 'image-width-responsive', 'image-width-fixed', 'image-height', 'resize-options',),
+        'BootstrapPicturePlugin': ('image-shapes', 'responsive-heights', 'image-size', 'resize-options',),
+    },
+    'bookmark_prefix': '/',
     'segmentation_mixins': (
         ('shop.cascade.segmentation.EmulateCustomerModelMixin', 'shop.cascade.segmentation.EmulateCustomerAdminMixin'),
     ),
