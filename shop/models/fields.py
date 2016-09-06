19dc04eb48a9484540298aa9a15fca016486921b
Hamza Khchine <hamzakhchine@gmail.com>
2016-09-06 18:16:37 +0100

    Add control over psycopg2's version (need to be bigger or equal than 2.5.4)

--- shop/models/fields.py
+++ shop/models/fields.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from distutils.version import LooseVersion
+import re
 from django.db import connection
 
 POSTGRES_FLAG = False
 if str(connection.vendor) == 'postgresql':
     POSTGRES_FLAG = True
-
 try:
-    if POSTGRES_FLAG:
+    import psycopg2
+
+    version = re.search('([0-9.]+)', psycopg2.__version__ or "0").group(0)
+    if POSTGRES_FLAG and (LooseVersion(version) >= LooseVersion('2.5.4')):
         from django.contrib.postgres.fields import JSONField
     else:
         raise ImportError
