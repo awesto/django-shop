57fc812d5cb3e8d08d24a9f2c54954b8c7a0b8f7
Hamza Khchine <hamzakhchine@gmail.com>
2016-09-06 18:55:20 +0100

    Add a comment about PostgreSQL's version which should be ≥ 9.4

--- shop/models/fields.py
+++ shop/models/fields.py
@@ -12,6 +12,8 @@ try:
     import psycopg2
 
     version = re.search('([0-9.]+)', psycopg2.__version__ or "0").group(0)
+    # To be able to use the Django version of JSONField, it requires to have PostgreSQL ≥ 9.4 , otherwise some issues
+    #  could be faced.
     if POSTGRES_FLAG and (LooseVersion(version) >= LooseVersion('2.5.4')):
         from django.contrib.postgres.fields import JSONField
     else:
