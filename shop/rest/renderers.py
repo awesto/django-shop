7818a662bc30c2a26d8047bf635b7c8571c028a9
René Fleschenberg <rene@fleschenberg.net>
2016-09-07 23:55:45 +0200

    Fix renderer

--- shop/rest/renderers.py
+++ shop/rest/renderers.py
@@ -14,26 +14,23 @@ class CMSPageRenderer(renderers.TemplateHTMLRenderer):
     are not compatible with this renderer.
     """
     def render(self, data, accepted_media_type=None, context=None):
-        request = context.pop('request')
-        response = context.pop('response')
-        context.pop('args')
-        context.pop('kwargs')
+        request = context['request']
+        response = context['response']
 
         if response.exception:
             template = self.get_exception_template(response)
         else:
-            view = context.pop('view')
+            view = context['view']
             template_names = self.get_template_names(response, view)
             template = self.resolve_template(template_names)
             context['paginator'] = view.paginator
             # set edit_mode, so that otherwise invisible placeholders can be edited inline
             context['edit_mode'] = request.current_page.publisher_is_draft
-
-        context['data'] = data
-        request_context = self.resolve_context(context, request, response)
-        return template.render(request_context)
-
-    def resolve_context(self, context, request, response):
-        if response.exception:
-            context['status_code'] = response.status_code
-        return RequestContext(request._request, context)
+        try:
+            # DRF >= 3.4.2
+            template_context = self.get_template_context(context, context)
+        except AttributeError:
+            # Fallback for DRF < 3.4.2
+            template_context = self.resolve_context({}, request, response)
+        template_context['data'] = data
+        return template.render(template_context, request=request)
