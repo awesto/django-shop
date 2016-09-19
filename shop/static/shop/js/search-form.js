93dbacdfbbe9ef532e73b1f8734351f929496a5d
Jacob Rief <jacob.rief@gmail.com>
2016-09-19 16:03:24 +0200

    django.shop.search requires django.shop.utils

--- shop/static/shop/js/search-form.js
+++ shop/static/shop/js/search-form.js
@@ -2,21 +2,21 @@
 'use strict';
 
 //module: django.shop, TODO: move this into a summary JS file
-var djangoShopModule = angular.module('django.shop.search', []);
+var djangoShopModule = angular.module('django.shop.search', ['django.shop.utils']);
 
 // Directive <form shop-product-search ...> to be used in the form containing the input field
 // for entering the search query
-djangoShopModule.directive('shopProductSearch', ['$window', '$location', function($window, $location) {
+djangoShopModule.directive('shopProductSearch', ['$location', 'djangoShop', function($location, djangoShop) {
 	return {
 		require: 'form',
 		restrict: 'AC',
 		controller: ['$scope', '$timeout', function($scope, $timeout) {
 			var acPromise = null;
 
-			// handle autocomplete
+			// handle typeahead using autocomplete
 			$scope.autocomplete = function() {
 				var config;
-				if ($scope.searchQuery.length < 3) {
+				if (!angular.isString($scope.searchQuery) || $scope.searchQuery.length < 3) {
 					config = null;
 					$location.search({});
 				} else {
@@ -37,34 +37,28 @@ djangoShopModule.directive('shopProductSearch', ['$window', '$location', functio
 					$scope.$emit('shopCatalogSearch', config);
 				}, 666);
 			};
-
 		}],
 		link: function(scope, element, attrs, formController) {
-			var i, splitted, queries = {params: $location.search()};
+			var queries = {params: $location.search()};
 
 			// convert query string from URL to object
 			if (angular.equals({}, queries.params)) {
-				// TODO: refactor by using djangoShop.paramsFromSearchQuery()
-				scope.searchQuery = '';
-				queries = $window.location.search.replace(/^\?/, '').split('&');
-				for (i = 0; i < queries.length; i++) {
-					splitted = queries[i].split('=');
-					if (splitted[0] === 'q') {
-						scope.searchQuery = decodeURIComponent(splitted[1].split('+').join('%20'));
-					}
-				}
+				queries = djangoShop.paramsFromSearchQuery();
+				scope.searchQuery = queries.q;
 			} else {
+				//$timeout(function() {
+					// delay until next digest cycle
+				//	scope.$emit('shopCatalogSearch', queries);
+				//});
 				scope.searchQuery = queries.params.q;
-				scope.autocomplete();
 			}
 
-			// handle classic search submission through form
+			// handle classic search through submission form
 			scope.submitSearch = function() {
 				if (scope.searchQuery.length > 1) {
 					element[0].submit();
 				}
 			};
-
 		}
 	};
 }]);
