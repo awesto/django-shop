a7adde7ebcdeff18d40759db9af89d87787cdc83
Jacob Rief <jacob.rief@gmail.com>
2016-09-22 17:29:47 +0200

    wizard is able to handle i18n_commodity

--- shop/forms/wizards.py
+++ shop/forms/wizards.py
@@ -2,13 +2,21 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.forms import models
+from django.db.models import Max
+from django.forms import models, fields, widgets
+from django.utils.translation import ugettext_lazy as _
 from cms.wizards.forms import BaseFormMixin
+from djangocms_text_ckeditor.fields import HTMLFormField
 from shop.models.related import ProductPageModel
 from shop.models.defaults.commodity import Commodity
 
 
 class CommodityWizardForm(BaseFormMixin, models.ModelForm):
+    product_name = fields.CharField(label=_("Product Name"),
+                                    widget=widgets.TextInput(attrs={'size': 50}))
+    slug = fields.CharField(label=_("Slug"), widget=widgets.TextInput(attrs={'size': 50}))
+    caption = HTMLFormField(label=_("Caption"), required=False)
+
     class Meta:
         model = Commodity
         fields = ('product_name', 'slug', 'caption', 'product_code',
@@ -36,6 +44,10 @@ class CommodityWizardForm(BaseFormMixin, models.ModelForm):
         return media
 
     def save(self, commit=True):
+        self.instance.product_name = self.cleaned_data['product_name']
+        self.instance.caption = self.cleaned_data['caption']
+        self.instance.slug = self.cleaned_data['slug']
+        self.instance.order = Commodity.objects.aggregate(max=Max('order'))['max'] + 1
         commodity = super(CommodityWizardForm, self).save(commit)
         ProductPageModel.objects.create(product=commodity, page=self.page.get_public_object())
         return commodity
