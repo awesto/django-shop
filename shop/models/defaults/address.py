420b414e0e42636aa14713c41318daf5ffa15749
Jacob Rief <jacob.rief@gmail.com>
2016-11-10 15:06:27 +0100

    updated docs

--- shop/models/defaults/address.py
+++ shop/models/defaults/address.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
 from shop.models.address import BaseShippingAddress, BaseBillingAddress, ISO_3166_CODES
 
 
