51dd18d03db514c6227ecbdf444a2dff114cbd2b
Jacob Rief <jacob.rief@gmail.com>
2016-11-13 20:53:26 +0100

    OrderItemSerializer now is configurable too

--- shop/rest/bases.py
+++ shop/rest/bases.py
@@ -1,8 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core import exceptions
+
 from rest_framework import serializers
+
 from shop.models.customer import CustomerModel
+from shop.models.order import OrderItemModel
+from shop.rest.money import MoneyField
+
+__all__ = ['BaseCustomerSerializer', 'OrderItemSerializer', 'get_product_summary_serializer_class',
+           'set_product_summary_serializer_class']
+
+_product_summary_serializer_class = None
+
+def get_product_summary_serializer_class():
+    """
+    Returns the the common ProductSerializer.
+    """
+    return _product_summary_serializer_class
+
+
+def set_product_summary_serializer_class(product_summary_serializer_class):
+    """
+    Sets the common ProductSerializer. This operation can be performed only once.
+    """
+    global _product_summary_serializer_class
+
+    if _product_summary_serializer_class:
+        msg = "Class `{}` inheriting from `ProductSummarySerializerBase` already registred."
+        raise exceptions.ImproperlyConfigured(msg.format(product_summary_serializer_class.__name__))
+    _product_summary_serializer_class = product_summary_serializer_class
 
 
 class BaseCustomerSerializer(serializers.ModelSerializer):
@@ -11,3 +39,11 @@ class BaseCustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomerModel
         fields = ('number', 'first_name', 'last_name', 'email')
+
+
+class BaseOrderItemSerializer(serializers.ModelSerializer):
+    line_total = MoneyField()
+    unit_price = MoneyField()
+
+    class Meta:
+        model = OrderItemModel
