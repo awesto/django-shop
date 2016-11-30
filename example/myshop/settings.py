4675e5d20ad604ef1b244b63700bd1d93214d43d
Jacob Rief <jacob.rief@gmail.com>
2016-11-30 16:33:47 +0100

    upgrading versus django-CMS=3.4.1

--- example/myshop/settings.py
+++ example/myshop/settings.py
@@ -444,7 +444,7 @@ CMSPLUGIN_CASCADE = {
         'cmsplugin_cascade.link.plugin_base.LinkElementMixin',
         'shop.cascade.plugin_base.CatalogLinkForm',
     ),
-    'alien_plugins': ('TextPlugin', 'TextLinkPlugin',),
+    'alien_plugins': ('TextPlugin', 'TextLinkPlugin', 'AcceptConditionPlugin',),
     'bootstrap3': {
         'template_basedir': 'angular-ui',
     },
