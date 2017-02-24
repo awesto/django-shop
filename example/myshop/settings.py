c56c2e780e445ba689ca15261b8a5020be6bb0a1
Jacob Rief <jacob.rief@gmail.com>
2017-02-24 23:00:36 +0100

    use lists instead of tuples

--- example/myshop/settings.py
+++ example/myshop/settings.py
@@ -338,7 +338,7 @@ REST_FRAMEWORK = {
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #   'rest_framework.authentication.TokenAuthentication',
     # ),
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 12,
 }
