24e2bfe184274b116f4e3b304b56761b55bf1b1e
René Fleschenberg <rene@fleschenberg.net>
2017-03-09 15:32:35 +0100

    Fix an AttributeError on checkout

--- shop/forms/checkout.py
+++ shop/forms/checkout.py
@@ -69,7 +69,7 @@ class GuestForm(UniqueEmailValidationMixin, DialogModelForm):
     def form_factory(cls, request, data, cart):
         customer_form = cls(data=data, instance=request.customer.user)
         if customer_form.is_valid():
-            customer_form.instance.recognize_as_guest(request, commit=False)
+            customer_form.instance.customer.recognize_as_guest(request, commit=False)
             customer_form.save()
         return customer_form
 
