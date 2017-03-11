ca1d12e41815226bb96e8b2f505537b02f479891
Jacob Rief <jacob.rief@gmail.com>
2017-03-11 23:07:29 +0100

    added missing whitespace

--- example/myshop/admin/i18n_polymorphic.py
+++ example/myshop/admin/i18n_polymorphic.py
@@ -6,12 +6,16 @@ from django.db.models import Max
 from django.template.context import Context
 from django.template.loader import get_template
 from django.utils.translation import ugettext_lazy as _
+
 from adminsortable2.admin import SortableAdminMixin, PolymorphicSortableAdminMixin
+
 from cms.admin.placeholderadmin import PlaceholderAdminMixin, FrontendEditableAdminMixin
 from parler.admin import TranslatableAdmin
 from polymorphic.admin import (PolymorphicParentModelAdmin, PolymorphicChildModelAdmin,
                                PolymorphicChildModelFilter)
+
 from shop.admin.product import CMSPageAsCategoryMixin, ProductImageInline, CMSPageFilter
+
 from myshop.models.polymorphic.product import Product
 from myshop.models.polymorphic.commodity import Commodity
 from myshop.models.polymorphic.smartcard import SmartCard
@@ -57,7 +61,6 @@ class SmartCardAdmin(SortableAdminMixin, TranslatableAdmin, FrontendEditableAdmi
     filter_horizontal = ('cms_pages',)
     inlines = (ProductImageInline,)
     prepopulated_fields = {'slug': ('product_name',)}
-
 
 admin.site.register(OperatingSystem, admin.ModelAdmin)
 
