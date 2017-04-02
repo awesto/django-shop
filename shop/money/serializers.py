f3eeb4cda667b9547ec5aaa26280e8b2c414d6f9
Jacob Rief <jacob.rief@gmail.com>
2017-04-02 15:57:33 +0200

    reexport Deserializer

--- shop/money/serializers.py
+++ shop/money/serializers.py
@@ -5,8 +5,10 @@ Override django.core.serializers.json.Serializer which renders our MoneyType as 
 from __future__ import unicode_literals
 import json
 from django.core.serializers.json import DjangoJSONEncoder, Serializer as DjangoSerializer
-from django.core.serializers.json import Deserializer  # nopyflakes
+from django.core.serializers.json import Deserializer
 from .money_maker import AbstractMoney
+
+__all__ = ['Serializer', 'Deserializer']
 
 
 class JSONEncoder(DjangoJSONEncoder):
