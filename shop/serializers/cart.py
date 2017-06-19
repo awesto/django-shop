7e972e0c5cdcbe23c7d23d7a61321f4776c7d5f8
Jacob Rief <jacob.rief@gmail.com>
2017-06-19 17:37:26 +0200

    readability counts

--- shop/serializers/cart.py
+++ shop/serializers/cart.py
@@ -15,10 +15,14 @@ class ExtraCartRow(serializers.Serializer):
     This data structure holds extra information for each item, or for the whole cart, while
     processing the cart using their modifiers.
     """
-    label = serializers.CharField(read_only=True,
-        help_text="A short description of this row in a natural language.")
-    amount = MoneyField(read_only=True,
-        help_text="The price difference, if applied.")
+    label = serializers.CharField(
+        read_only=True,
+        help_text="A short description of this row in a natural language.",
+    )
+
+    amount = MoneyField(
+        help_text="The price difference, if applied.",
+    )
 
 
 class ExtraCartRowList(serializers.Serializer):
