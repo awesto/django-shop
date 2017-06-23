ed482046ebed1538b6c3a0e424c4a5e6991f2e88
Jacob Rief <jacob.rief@gmail.com>
2017-06-23 16:53:20 +0200

    simplified forms-set directives

--- shop/static/shop/js/forms-set.js
+++ shop/static/shop/js/forms-set.js
@@ -4,20 +4,23 @@
 var djangoShopModule = angular.module('django.shop.forms-set', ['djng.urls', 'djng.forms']);
 
 
-// Directive <ANY shop-forms-set ...>
+// Directive <ANY shop-forms-set upload-url="/rest/endpoint" ...>, optionally with a REST endpoint.
 // Use this as a wrapper around self validating <form ...> elements (see directive below), so that
 // we can disable the proceed button whenever one of those forms does not validate.
 // For dialog forms, such a proceed button can be rendered as:
-// <button shop-dialog-proceed ng-click="proceedWith('PURCHASE_NOW')" ng-disabled="stepIsValid===false">Purchase Now</button>
+// <button ng-click="update('PURCHASE_NOW')" ng-disabled="stepIsValid===false">Submit</button>
 djangoShopModule.directive('shopFormsSet', function() {
 	return {
 		require: 'shopFormsSet',
 		scope: true,
-		controller: ['$scope', function($scope) {
+		controller: ['$scope', '$http', '$window', 'djangoForm', function($scope, $http, $window, djangoForm) {
 			var self = this;
 
 			// a map of booleans keeping the validation state for each of the child forms
 			this.digestValidatedForms = {};
+
+			// dictionary of form names mapping their model scopes
+			this.digestUploadScope = {};
 
 			// check each child form's $valid state and reduce it to one single state scope.stepIsValid
 			this.reduceValidation = function(formId, formIsValid) {
@@ -27,28 +30,22 @@ djangoShopModule.directive('shopFormsSet', function() {
 					$scope.stepIsValid = $scope.stepIsValid && validatedForm;
 				});
 			};
-		}]
-	};
-});
-
-
-// Directive <ANY shop-forms-endpoint ...>
-// Create a summary of one or more model scopes to be submitted to the given endpoint
-djangoShopModule.directive('shopFormsEndpoint', ['$http', '$window', 'djangoForm', function($http, $window, djangoForm) {
-	return {
-		restrict: 'A',
-		require: 'shopFormsEndpoint',
-		scope: true,
-		controller: ['$scope', function($scope) {
-			var self = this;
-			this.digestUploadScope = {};
 
 			this.uploadScope = function(method, action, extraData) {
+				if (!self.uploadURL)
+					throw new Error("Can not upload form data: Missing attribute 'upload-url'");
+
 				// merge the data from various scope entities into one data object
 				var data = {};
-				angular.merge(data, extraData);
+				if (extraData) {
+					angular.merge(data, extraData);
+				}
 				angular.forEach(self.digestUploadScope, function(scopeModel) {
-					angular.merge(data, $scope.$eval(scopeModel));
+					var modelScope = {}, value = $scope.$eval(scopeModel);
+					if (value) {
+						modelScope[scopeModel] = value;
+						angular.merge(data, modelScope);
+					}
 				});
 
 				// submit data from all forms below this endpoint to the server
@@ -75,18 +72,23 @@ djangoShopModule.directive('shopFormsEndpoint', ['$http', '$window', 'djangoForm
 				});
 			};
 		}],
-		link: function(scope, element, attrs, formEndpointController) {
-			if (!attrs.shopFormsEndpoint)
-				throw new Error("Attribute 'shop-forms-endpoint' does not declare any endpoint.");
-			formEndpointController.uploadURL = attrs.shopFormsEndpoint;
+		link: function(scope, element, attrs, formsSetController) {
+			if (attrs.shopUploadUrl) {
+				formsSetController.uploadURL = attrs.shopUploadUrl;
+			}
 		}
 	};
-}]);
+});
 
 
 // This directive enriches AngularJS's internal form-controller if it is wrapped inside a
-// <ANY shop-forms-set ...> directive. Its only purpose is to summarize the validity of the given forms,
-// so that buttons rendered outside of the forms can check the ``stepIsValid`` attribute.
+// <ANY shop-forms-set ...> directive. One purpose is to summarize the validity of the given forms, so that buttons
+// rendered outside of the <form ...> element but inside the <ANY shop-forms-set ...> element can check the
+// ``stepIsValid`` attribute.
+// Another purpose of this directive is to summarize the scope-models of the given forms, so that the scope can
+// be uploaded to the endpoint URL using one submission.
+// Usage: <form name="my_name" scope-model="my_data" novalidate> where `my_data` is used to access the form's
+// data inside the scope.
 djangoShopModule.directive('form', ['$timeout', function($timeout) {
 	return {
 		restrict: 'E',
@@ -97,6 +99,10 @@ djangoShopModule.directive('form', ['$timeout', function($timeout) {
 
 			if (!formsSetController)
 				return;  // not for forms outside <ANY shop-forms-set></ANY shop-forms-set>
+
+			if (attrs.name && attrs.scopeModel) {
+				formsSetController.digestUploadScope[attrs.name] = attrs.scopeModel;
+			}
 
 			// create new isolated scope for this form
 			scope = scope.$new(true);
@@ -123,45 +129,32 @@ djangoShopModule.directive('form', ['$timeout', function($timeout) {
 }]);
 
 
-// This directive enriches AngularJS's internal form-controller if it is wrapped inside a
-// <ANY shop-forms-endpoint ...> directive. Its only purpose is to summarize the scope-models
-// of the given forms, so that the scope can be uploaded to the endpoint using one submission.
-// Usage: <form name="my_name" scope-model="my_data" novalidate> where `my_data` is used to
-// access the form's data inside the scope.
-djangoShopModule.directive('form', function() {
+
+// This directive enriches the button element if it is wrapped inside a <ANY shop-forms-set ...> directive.
+// It adds to functions to its scope ``create`` and ``update`` which shall be used to invoke a POST or
+// PUT request on the forms-set endpoint URL.
+// Optionally one can add ``shop-extra-data="..."`` to this button element, in order to pass further information
+// to the given endpoint.
+djangoShopModule.directive('button', function() {
 	return {
 		restrict: 'E',
-		require: ['^?shopFormsEndpoint', 'form'],
-		priority: 2,
-		link: function(scope, element, attrs, controllers) {
-			var formsEndpointController = controllers[0], formController = controllers[1];
-
-			if (!formsEndpointController || !attrs.scopeModel)
-				return;  // not for forms outside <ANY shop-forms-endpoint> element
-
-			formsEndpointController.digestUploadScope[attrs.name] = attrs.scopeModel;
-		}
-	};
-});
-
-
-djangoShopModule.directive('shopEndpointProceed', function() {
-	return {
-		restrict: 'EA',
-		require: '^shopFormsEndpoint',
+		require: '^?shopFormsSet',
 		scope: true,
-		link: function(scope, element, attrs, formsEndpointController) {
-			if (attrs.extraData) {
-				scope.extraData = scope.$eval(attrs.extraData) || attrs.extraData;
-			}
+		link: function(scope, element, attrs, formsSetController) {
+			if (!formsSetController)
+				return;  // not for buttons outside <ANY shop-forms-set></ANY shop-forms-set>
 
 			scope.create = function(action) {
-				formsEndpointController.uploadScope('POST', action, scope.extraData);
+				formsSetController.uploadScope('POST', action, scope.shopExtraData);
 			};
 
 			scope.update = function(action) {
-				formsEndpointController.uploadScope('PUT', action, scope.extraData);
+				formsSetController.uploadScope('PUT', action, scope.shopExtraData);
 			};
+
+			if (attrs.shopExtraData) {
+				scope.shopExtraData = scope.$eval(attrs.shopExtraData) || attrs.shopExtraData;
+			}
 		}
 	};
 });
