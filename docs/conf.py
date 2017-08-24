0e66eab3289fba882386ac465417bdd3150ea492
Jacob Rief <jacob.rief@gmail.com>
2017-08-24 16:42:44 +0200

    Retry to build Sphix docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,12 +14,20 @@
 import os
 import sys
 import datetime
-from django.conf import settings
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../example'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myshop.settings'
+os.environ['DJANGO_SHOP_TUTORIAL'] = 'commodity'
+
+print(sys.path)
+from django.conf import settings
+import django
+django.setup()
 
 # -- General configuration -----------------------------------------------------
 
