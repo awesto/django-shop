3785153568d819ed76dacdb4677b5e8c11d43f1d
Jacob Rief <jacob.rief@gmail.com>
2017-10-10 18:47:10 +0200

    adopted methods digest and delete for Billing Address

--- shop/views/address.py
+++ shop/views/address.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.renderers import BrowsableAPIRenderer
 
+from shop.models.address import ShippingAddressModel, BillingAddressModel
 from shop.models.cart import CartModel
 from shop.rest.money import JSONRenderer
 
@@ -30,14 +31,20 @@ class AddressEditView(GenericAPIView):
             form = self.form_class()
         else:
             try:
-                instance = request.customer.shippingaddress_set.get(priority=priority)
-            except CartModel.DoesNotExist:
-                return Response(status=404)
+                if self.form_class.__name__ == 'BillingAddressForm':
+                    instance = request.customer.billingaddress_set.get(priority=priority)
+                elif self.form_class.__name__ == 'ShippingAddressForm':
+                    instance = request.customer.shippingaddress_set.get(priority=priority)
+                else:
+                    raise CartModel.DoesNotExist()
+            except (ShippingAddressModel.DoesNotExist, BillingAddressModel.DoesNotExist):
+                return Response(status=410)
             else:
                 cart = CartModel.objects.get_from_request(request)
                 form = self.form_class(instance=instance, cart=cart)
         initial_data = dict((k, v) for k, v in form.get_initial_data().items() if k in self.visible_fields)
-        return Response({'shipping_address_form': initial_data})
+        initial_data.pop('use_primary_address', None)
+        return Response({self.form_class.form_name: initial_data})
 
     def put(self, request, *args, **kwargs):
         data = json.loads(request.body).get(self.form_class.scope_prefix, {})
@@ -50,14 +57,20 @@ class AddressEditView(GenericAPIView):
     def delete(self, request, priority=None, *args, **kwargs):
         if priority != 'add':
             try:
-                request.customer.shippingaddress_set.get(priority=priority).delete()
-            except CartModel.DoesNotExist:
-                return Response(status=404)
+                if self.form_class.__name__ == 'BillingAddressForm':
+                    request.customer.billingaddress_set.get(priority=priority).delete()
+                elif self.form_class.__name__ == 'ShippingAddressForm':
+                    request.customer.shippingaddress_set.get(priority=priority).delete()
+            except (ShippingAddressModel.DoesNotExist, BillingAddressModel.DoesNotExist):
+                pass
 
         # take the last of the remaining addresses
-        instance = request.customer.shippingaddress_set.last()
+        if self.form_class.__name__ == 'BillingAddressForm':
+            instance = request.customer.billingaddress_set.last()
+        elif self.form_class.__name__ == 'ShippingAddressForm':
+            instance = request.customer.shippingaddress_set.last()
         cart = CartModel.objects.get_from_request(request)
         form = self.form_class(instance=instance, cart=cart)
         initial_data = dict((k, v) for k, v in form.get_initial_data().items() if k in self.visible_fields)
         initial_data.update(active_priority=str(instance.priority), siblings_summary=form.siblings_summary)
-        return Response({'shipping_address_form': initial_data})
+        return Response({self.form_class.form_name: initial_data})
