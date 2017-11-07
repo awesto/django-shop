2a7fb05f5c31f5a3d3257516f931c3557d71cf6d
Jacob Rief <jacob.rief@gmail.com>
2017-11-07 08:47:15 +0100

    restore transition target: pack_goods

--- shop/shipping/workflows.py
+++ shop/shipping/workflows.py
@@ -19,6 +19,7 @@ class CommissionGoodsWorkflowMixin(object):
     """
     TRANSITION_TARGETS = {
         'pick_goods': _("Picking goods"),
+        'pack_goods': _("Packing goods"),
         'ready_for_delivery': _("Ready for delivery"),
     }
 
@@ -27,7 +28,7 @@ class CommissionGoodsWorkflowMixin(object):
     def pick_goods(self, by=None):
         """Change status to 'pick_goods'."""
 
-    @transition(field='status', source=['pick_goods'], target='ready_for_delivery',
+    @transition(field='status', source=['pick_goods'], target='pack_goods',
                 custom=dict(admin=True, button_name=_("Pack the goods")))
     def pack_goods(self, by=None):
         """Change status to 'pack_goods'."""
@@ -43,6 +44,7 @@ class PartialDeliveryWorkflowMixin(object):
     """
     TRANSITION_TARGETS = {
         'pick_goods': _("Picking goods"),
+        'pack_goods': _("Packing goods"),
         'ready_for_delivery': _("Ready for delivery"),
     }
 
@@ -55,11 +57,11 @@ class PartialDeliveryWorkflowMixin(object):
                 unfulfilled_items += order_item.quantity - (aggr['delivered'] or 0)
         return unfulfilled_items
 
-    def ready_for_delivery(self):
+    def ready_for_picking(self):
         return self.is_fully_paid() and self.unfulfilled_items > 0
 
     @transition(field='status', source=['payment_confirmed', 'ready_for_delivery', 'ship_goods'],
-                target='pick_goods', conditions=[ready_for_delivery],
+                target='pick_goods', conditions=[ready_for_picking],
                 custom=dict(admin=True, button_name=_("Pick the goods")))
     def pick_goods(self, by=None):
         """Change status to 'pick_goods'."""
@@ -67,7 +69,7 @@ class PartialDeliveryWorkflowMixin(object):
         if shipping_method:
             DeliveryModel.objects.get_or_create(order=self, defaults={'shipping_method': shipping_method})
 
-    @transition(field='status', source=['pick_goods'], target='ready_for_delivery',
+    @transition(field='status', source=['pick_goods'], target='pack_goods',
                 custom=dict(admin=True, button_name=_("Pack the goods")))
     def pack_goods(self, by=None):
         """Prepare shipping object and change status to 'pack_goods'."""
