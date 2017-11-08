5c7e3b680b4a1d79e199b8b66e0bef6aa0762006
Jacob Rief <jacob.rief@gmail.com>
2017-11-08 15:33:32 +0100

    remove unused imports

--- shop/cascade/checkout.py
+++ shop/cascade/checkout.py
@@ -6,10 +6,9 @@ from django.forms.fields import CharField
 from django.forms import widgets
 from django.template import engines
 from django.template.loader import select_template
-from django.utils.html import strip_tags, format_html
+from django.utils.html import format_html
 from django.utils.module_loading import import_string
 from django.utils.safestring import mark_safe
-from django.utils.text import Truncator
 from django.utils.translation import ugettext_lazy as _, pgettext_lazy
 try:
     from html.parser import HTMLParser  # py3
@@ -17,13 +16,10 @@ except ImportError:
     from HTMLParser import HTMLParser  # py2
 from cms.plugin_pool import plugin_pool
 
-from djangocms_text_ckeditor.widgets import TextEditorWidget
-from djangocms_text_ckeditor.utils import plugin_tags_to_user_html
 from djangocms_text_ckeditor.cms_plugins import TextPlugin
 
 from cmsplugin_cascade.fields import GlossaryField
 from cmsplugin_cascade.strides import strides_plugin_map, strides_element_map, TextStridePlugin, TextStrideElement
-from cmsplugin_cascade.link.cms_plugins import TextLinkPlugin
 from cmsplugin_cascade.link.forms import LinkForm, TextLinkFormMixin
 from cmsplugin_cascade.link.plugin_base import LinkElementMixin
 from cmsplugin_cascade.plugin_base import TransparentContainer
