93717a35b6231f19cd9ab595603ca01fa0ed4a07
Jacob Rief <jacob.rief@gmail.com>
2018-02-02 23:45:40 +0100

    More translations

--- example/myshop/management/commands/fix_filer_bug_965.py
+++ example/myshop/management/commands/fix_filer_bug_965.py
@@ -2,13 +2,12 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
-from django.utils.translation import ugettext_lazy as _
 
 from filer.models.imagemodels import Image
 
 
 class Command(BaseCommand):
-    help = _("Fix https://github.com/divio/django-filer/issues/965")
+    help = "Fix https://github.com/divio/django-filer/issues/965"
 
     def handle(self, verbosity, *args, **options):
         for img in Image.objects.all():
