e655a54551fc0caed57e6de2199548b679c2891f
Jacob Rief <jacob.rief@gmail.com>
2018-11-03 16:28:20 +0100

    Fix styling and ordering of product gallery

--- shop/cascade/catalog.py
+++ shop/cascade/catalog.py
@@ -79,8 +79,11 @@ plugin_pool.register_plugin(ShopAddToCartPlugin)
 
 
 class ProductGalleryForm(ModelForm):
-    product = ProductSelectField(required=False, label=_("Related Product"),
-        help_text=_("Choose related product"))
+    product = ProductSelectField(
+        required=False,
+        label=_("Related Product"),
+        help_text=_("Choose related product"),
+    )
 
     class Meta:
         exclude = ('glossary',)
@@ -153,7 +156,8 @@ class ShopProductGallery(WithSortableInlineElementsMixin, ShopPluginBase):
         queryset = ProductModel.objects.filter(pk__in=product_ids, active=True)
         serializer_class = app_settings.PRODUCT_SUMMARY_SERIALIZER
         serialized = serializer_class(queryset, many=True, context={'request': context['request']})
-        context['products'] = serialized.data
+        # sort the products according to the order provided by `sortinline_elements`.
+        context['products'] = [product for id in product_ids for product in serialized.data if product['id'] == id]
         return context
 
 plugin_pool.register_plugin(ShopProductGallery)
