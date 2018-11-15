cf491e267188f73cac5dd2cbb9165385f0b24b9d
Jacob Rief <jacob.rief@gmail.com>
2018-11-15 21:57:27 +0100

    linking button using template with responsive feedback

--- shop/cascade/settings.py
+++ shop/cascade/settings.py
@@ -2,18 +2,25 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 from cmsplugin_cascade.bootstrap4.mixins import BootstrapUtilities
 
 
 CASCADE_PLUGINS = getattr(settings, 'SHOP_CASCADE_PLUGINS',
-    ('auth', 'breadcrumb', 'catalog', 'cart', 'checkout', 'extensions', 'order', 'processbar', 'search',))
+    ['auth', 'breadcrumb', 'catalog', 'cart', 'checkout', 'extensions', 'order', 'processbar', 'search']
+)
 
 
 def set_defaults(config):
     config.setdefault('plugins_with_extra_mixins', {})
+    config.setdefault('plugins_with_extra_render_templates', {})
     config['plugins_with_extra_mixins'].setdefault('ShopReorderButtonPlugin', BootstrapUtilities(
         BootstrapUtilities.margins,
     ))
     config['plugins_with_extra_mixins'].setdefault('ShopCancelOrderButtonPlugin', BootstrapUtilities(
         BootstrapUtilities.margins,
     ))
+    config['plugins_with_extra_render_templates'].setdefault('BootstrapButtonPlugin', [
+        ('shop/button.html', _("Responsive Feedback")),
+        ('cascade/bootstrap4/button.html', _("Default")),
+    ])
