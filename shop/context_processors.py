c9875b740c15e85a542f15a603a31325fea4b8e8
Jacob Rief <jacob.rief@gmail.com>
2018-11-27 09:26:49 +0100

    refactor template context processor ng_model_options -> shop_settings

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -24,9 +24,9 @@ def customer(request):
     return context
 
 
-def ng_model_options(request):
+def shop_settings(request):
     """
-    Add ng-model-options to the context, since these settings must be configurable
+    Add configuration settings to the context to customize the shop's settings in templates
     """
     return {
         'EDITCART_NG_MODEL_OPTIONS': app_settings.EDITCART_NG_MODEL_OPTIONS,
