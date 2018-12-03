c0e56e939ba9f585114e0e9758da7580445bff33
Jacob Rief <jacob.rief@gmail.com>
2018-12-03 14:14:45 +0100

    don’t try to convert HTML into plain-text emails

--- shop/models/notification.py
+++ shop/models/notification.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from bs4 import BeautifulSoup
-
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
-from django.core.mail import EmailMessage, EmailMultiAlternatives
 from django.db import models
 from django.db.models import Q
 from django.http.request import HttpRequest
-from django.template import engines
 from django.utils.six.moves.urllib.parse import urlparse
-from django.utils.translation import ugettext_lazy as _, override as translation_override, ugettext_noop
+from django.utils.translation import ugettext_lazy as _, ugettext_noop
 
 from post_office import mail
-from post_office.models import Email as OriginalEmail, EmailTemplate
+from post_office.models import EmailTemplate
 
 from filer.fields.file import FilerFileField
 
@@ -22,52 +18,6 @@ from shop.conf import app_settings
 from shop.models.order import BaseOrder
 from shop.models.fields import ChoiceEnum, ChoiceEnumField
 from shop.signals import email_queued
-
-
-class Email(OriginalEmail):
-    """
-    Patch class `post_office.models.Email` which fixes https://github.com/ui/django-post_office/issues/116
-    and additionally converts HTML messages into plain text messages.
-    """
-    class Meta:
-        proxy = True
-
-    def email_message(self, connection=None):
-        if self.template is not None:
-            render_language = self.context.get('render_language', settings.LANGUAGE_CODE)
-            with translation_override(render_language):
-                subject = engines['django'].from_string(self.template.subject).render(self.context)
-                message = engines['django'].from_string(self.template.content).render(self.context)
-                html_message = engines['django'].from_string(self.template.html_content).render(self.context)
-        else:
-            subject = self.subject
-            message = self.message
-            html_message = self.html_message
-
-        if html_message:
-            if not message:
-                message = BeautifulSoup(html_message).text
-            mailmsg = EmailMultiAlternatives(
-                subject=subject, body=message, from_email=self.from_email,
-                to=self.to, bcc=self.bcc, cc=self.cc,
-                connection=connection, headers=self.headers)
-            mailmsg.attach_alternative(html_message, 'text/html')
-        else:
-            mailmsg = EmailMessage(
-                subject=subject, body=message, from_email=self.from_email,
-                to=self.to, bcc=self.bcc, cc=self.cc,
-                connection=connection, headers=self.headers)
-
-        for attachment in self.attachments.all():
-            mailmsg.attach(attachment.name, attachment.file.read())
-        return mailmsg
-
-
-class EmailManager(models.Manager):
-    def get_queryset(self):
-        return Email.objects.get_queryset()
-
-OriginalEmail.add_to_class('objects', EmailManager())
 
 
 class Notify(ChoiceEnum):
