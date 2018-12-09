e4ad9759072e6d0153173db6cdbbfb226e77bf39
Jacob Rief <jacob.rief@gmail.com>
2018-12-09 16:56:18 +0100

    WiP on check CMS pages

--- shop/management/commands/shop.py
+++ shop/management/commands/shop.py
@@ -22,26 +22,54 @@ class Command(BaseCommand):
         from shop.cms_apphooks import CatalogListCMSApp, CatalogSearchCMSApp, OrderApp, PasswordResetApp
         from cms.apphook_pool import apphook_pool
 
+        complains = []
+
+        # check for missing apphooks not registered by the merchant implementation
         required_apps = [CatalogListCMSApp, CatalogSearchCMSApp, OrderApp, PasswordResetApp]
-        apphooks = [(apphook_pool.get_apphook(ah), name) for ah, name in apphook_pool.get_apphooks()]
-        for apphook, name in apphooks:
+        for apphook, _ in apphook_pool.get_apphooks():
+            apphook = apphook_pool.get_apphook(apphook)
+            for required_app in required_apps:
+                if isinstance(apphook, required_app):
+                    required_apps.remove(required_app)
+                    break
+        for missing_app in required_apps:
+            msg = "The project must register an AppHook inheriting from '{}'"
+            complains.push(msg.format(missing_app.__name__))
+
+        # check for CMS pages requiring to be configured with an Application
+        missing_page_msg = "At least one CMS page should have an Application of type '{}' attached to it."
+        for apphook, name in apphook_pool.get_apphooks():
+            apphook = apphook_pool.get_apphook(apphook)
             if isinstance(apphook, CatalogListCMSApp):
-                required_apps.remove(CatalogListCMSApp)
                 catalog_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
                 if not catalog_pages.exists():
-                    msg = "At least one CMS page should have an Application of type '{}' attached to it."
-                    self.stdout.write(msg.format(name))
+                    complains.push(missing_page_msg.format(name))
 
             if isinstance(apphook, CatalogSearchCMSApp):
-                required_apps.remove(CatalogSearchCMSApp)
                 search_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
-                if not search_pages.exists():
-                    msg = "At least one CMS page should have an Application of type '{}' attached to it."
-                    self.stdout.write(msg.format(name))
+                if search_pages.exists():
+                    if not search_pages.filter(reverse_id='shop-search-product').exists():
+                        msg = "A CMS page implementing the Search Application must be referenced with ID: 'shop-search-product'."
+                        complains.push(msg)
+                else:
+                    complains.push(missing_page_msg.format(name))
 
             if isinstance(apphook, OrderApp):
-                required_apps.remove(OrderApp)
                 order_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
-                if not order_pages.exists():
-                    msg = "At least one CMS page should have an Application of type '{}' attached to it."
-                    self.stdout.write(msg.format(name))
+                if order_pages.exists():
+                    if not order_pages.filter(reverse_id='shop-order').exists():
+                        msg = "A CMS page implementing the Order Views must be referenced with ID: 'shop-order'."
+                        complains.push(msg)
+                    if not order_pages.filter(reverse_id='shop-order-last').exists():
+                        msg = "A CMS page implementing the Thank You Order must be referenced with ID: 'shop-order-last'."
+                        complains.push(msg)
+                else:
+                    complains.push(missing_page_msg.format(name))
+
+            if isinstance(apphook, PasswordResetApp):
+                password_reset_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
+                if not password_reset_pages.exists():
+                    complains.push(missing_page_msg.format(name))
+
+
+        self.stdout.write(msg.format(name))
