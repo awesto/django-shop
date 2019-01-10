ead639a522304ae08eb5e59ce1a9c001bf1afcb5
Jacob Rief <jacob.rief@gmail.com>
2019-01-10 12:00:30 +0100

    add test for tax cart-modifiers

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import AnonymousUser
-from django.urls import reverse
-from shop.models.cart import CartModel
-from shop.models.customer import CustomerModel
+from shop.conf import app_settings
 from shop.views.cart import CartViewSet, WatchViewSet
+from shop.modifiers.pool import CartModifiersPool
 import pytest
+
+CartModifiersPool.USE_CACHE = False
 
 
 @pytest.mark.django_db
@@ -80,3 +80,36 @@ def test_add_watch_item(api_rf, watch_list):
     watch_list.refresh_from_db()
     assert watch_list.num_items == 1
     assert watch_list.items.all()[0].quantity == 1
+
+
+@pytest.mark.django_db
+def test_include_tax_modifier(api_rf, cart, settings):
+    settings.SHOP_CART_MODIFIERS = [
+        'shop.modifiers.defaults.DefaultCartModifier',
+        'shop.modifiers.taxes.CartIncludeTaxModifier',
+    ]
+    request = api_rf.get('/shop/api/cart')
+    request.customer = cart.customer
+
+    response = CartViewSet.as_view({'get': 'list'})(request)
+    assert response.status_code == 200
+    assert response.data['subtotal'] == str(cart.subtotal)
+    tax_rate = 1 + app_settings.SHOP_VALUE_ADDED_TAX / 100
+    assert response.data['total'] == str(cart.total * tax_rate)
+
+
+@pytest.mark.django_db
+def test_exclude_tax_modifier(api_rf, cart, settings):
+    settings.SHOP_CART_MODIFIERS = [
+        'shop.modifiers.defaults.DefaultCartModifier',
+        'shop.modifiers.taxes.CartExcludedTaxModifier',
+    ]
+    request = api_rf.get('/shop/api/cart')
+    request.customer = cart.customer
+
+    response = CartViewSet.as_view({'get': 'list'})(request)
+    assert response.status_code == 200
+    assert response.data['subtotal'] == str(cart.subtotal)
+    assert len(response.data['extra_rows']) == 1
+    response.data['extra_rows'][0]['modifier'] == 'taxes'
+    assert response.data['total'] == str(cart.total)
