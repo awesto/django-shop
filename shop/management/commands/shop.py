655614beb9aa15cc1fc726d3ecd19c788de821b8
Jacob Rief <jacob.rief@gmail.com>
2019-01-29 00:39:11 +0100

    check all possible pages

--- shop/management/commands/shop.py
+++ shop/management/commands/shop.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.core.management.base import BaseCommand
-from django.utils.translation import ugettext_lazy as _
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.module_loading import import_string
+
+
+class MissingPage(CommandError):
+    """
+    Exception class indicating that a CMS page with a predefined ``reverse_id`` is missing.
+    """
+
+
+class MissingAppHook(CommandError):
+    """
+    Exception class indicating that a page misses the application.
+    """
+
+
+class MissingPlugin(CommandError):
+    """
+    Exception class indicating that a special plugin is missing or misconfigured on a given
+    CMS page.
+    """
 
 
 class Command(BaseCommand):
     help = _("Collect information about all customers which accessed this shop.")
 
     def add_arguments(self, parser):
-        parser.add_argument("subcommand", help=_("./manage.py shop [customers|]"))
+        parser.add_argument("subcommand", help="./manage.py shop [customers|check-pages]")
+        parser.add_argument("--add-missing")
 
     def handle(self, verbosity, subcommand, *args, **options):
         if subcommand == 'check-pages':
@@ -19,54 +40,77 @@ class Command(BaseCommand):
 
     def check_pages(self):
         from cms.models.pagemodel import Page
-        from shop.cms_apphooks import CatalogListCMSApp, CatalogSearchCMSApp, OrderApp, PasswordResetApp
-        from cms.apphook_pool import apphook_pool
 
         complains = []
+        apphook = self.get_installed_apphook('CatalogListCMSApp')
+        catalog_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
+        if not catalog_pages.exists():
+            msg = "There should be at least one published CMS page configured to use an Application inheriting from 'CatalogListCMSApp'."
+            complains.append(msg)
 
-        # check for missing apphooks not registered by the merchant implementation
-        required_apps = [CatalogListCMSApp, CatalogSearchCMSApp, OrderApp, PasswordResetApp]
+        page_attributes = [
+            ('shop-search-product', 'CatalogSearchCMSApp', 'ShopSearchResultsPlugin', {}),
+            ('shop-cart', None, 'ShopCartPlugin', {'render_type': 'editable'}),
+            ('shop-watch-list', None, 'ShopCartPlugin', {'render_type': 'watch'}),
+            ('shop-order', 'OrderApp', 'ShopOrderViewsPlugin', {}),
+            ('shop-customer-details', None, 'CustomerFormPlugin', {}),
+            ('shop-password-change', None, 'ShopAuthenticationPlugin', {'form_type': 'password-change'}),
+            ('password-reset-request', None, 'ShopAuthenticationPlugin', {'form_type': 'password-reset-request'}),
+            ('password-reset-confirm', 'PasswordResetApp', 'ShopAuthenticationPlugin', {'form_type': 'password-reset-confirm'}),
+        ]
+        for attribs in page_attributes:
+            try:
+                self.check_page_content(*attribs)
+            except CommandError as exc:
+                complains.append(str(exc))
+
+        if len(complains) > 0:
+            rows = [" {}. {}".format(id, msg) for id, msg in enumerate(complains, 1)]
+            rows.insert(0, "The following CMS pages must be fixed:")
+            msg = "\n".join(rows)
+            self.stdout.write(msg)
+
+    def get_installed_apphook(self, base_apphook_name):
+        from cms.apphook_pool import apphook_pool
+        base_apphook = import_string('shop.cms_apphooks.' + base_apphook_name)
+
         for apphook, _ in apphook_pool.get_apphooks():
             apphook = apphook_pool.get_apphook(apphook)
-            for required_app in required_apps:
-                if isinstance(apphook, required_app):
-                    required_apps.remove(required_app)
-                    break
-        for missing_app in required_apps:
-            msg = "The project must register an AppHook inheriting from '{}'"
-            complains.push(msg.format(missing_app.__name__))
+            if isinstance(apphook, base_apphook):
+                return apphook
+        else:
+            msg = "The project must register an AppHook inheriting from '{apphook_name}'"
+            raise MissingAppHook(msg.format(apphook_name=base_apphook_name))
 
-        # check for CMS pages requiring to be configured with an Application
-        missing_page_msg = "At least one CMS page should have an Application of type '{}' attached to it."
-        for apphook, name in apphook_pool.get_apphooks():
-            apphook = apphook_pool.get_apphook(apphook)
-            if isinstance(apphook, CatalogListCMSApp):
-                catalog_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
-                if not catalog_pages.exists():
-                    complains.push(missing_page_msg.format(name))
+    def check_page_content(self, reverse_id, base_apphook_name, plugin_type, subset):
+        from cms.apphook_pool import apphook_pool
+        from cms.models.pagemodel import Page
+        from cms.plugin_pool import plugin_pool
 
-            if isinstance(apphook, CatalogSearchCMSApp):
-                search_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
-                if search_pages.exists():
-                    if not search_pages.filter(reverse_id='shop-search-product').exists():
-                        msg = "A CMS page implementing the Search Application must be referenced with ID: 'shop-search-product'."
-                        complains.push(msg)
-                else:
-                    complains.push(missing_page_msg.format(name))
+        page = Page.objects.public().filter(reverse_id=reverse_id).first()
+        if not page:
+            msg = "There should be a published CMS page with a reference ID: '{reverse_id}'."
+            raise MissingPage(msg.format(reverse_id=reverse_id))
 
-            if isinstance(apphook, OrderApp):
-                order_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
-                if order_pages.exists():
-                    if not order_pages.filter(reverse_id='shop-order').exists():
-                        msg = "A CMS page implementing the Order Views must be referenced with ID: 'shop-order'."
-                        complains.push(msg)
-                else:
-                    complains.push(missing_page_msg.format(name))
+        if base_apphook_name:
+            apphook = self.get_installed_apphook(base_apphook_name)
+            if apphook_pool.get_apphook(page.application_urls) is not apphook:
+                msg = "Page on URL '{url}' must be configured to use Application inheriting from '{app_hook}'."
+                raise MissingAppHook(msg.format(url=page.get_absolute_url(), apphook_name=base_apphook_name))
 
-            if isinstance(apphook, PasswordResetApp):
-                password_reset_pages = Page.objects.public().filter(application_urls=apphook.__class__.__name__)
-                if not password_reset_pages.exists():
-                    complains.push(missing_page_msg.format(name))
+        placeholder = page.placeholders.filter(slot='Main Content').first()
+        if not placeholder:
+            msg = "Page on URL '{url}' does not contain any plugin."
+            raise MissingPlugin(msg.format(url=page.get_absolute_url()))
 
+        plugin_name = plugin_pool.get_plugin(plugin_type).name
+        for language in page.get_languages():
+            plugin = placeholder.cmsplugin_set.filter(plugin_type=plugin_type, language=language).first()
+            if not plugin:
+                msg = "Page on URL '{url}' shall contain a plugin named '{plugin_name}'."
+                raise MissingPlugin(msg.format(url=page.get_absolute_url(), plugin_name=plugin_name))
 
-        self.stdout.write(msg.format(name))
+            glossary_items = plugin.get_bound_plugin().glossary.items()
+            if not all(item in glossary_items for item in subset.items()):
+                msg = "Plugin named '{plugin_name}' on page with URL '{url}' is misconfigured."
+                raise MissingPlugin(msg.format(url=page.get_absolute_url(), plugin_name=plugin_name))
