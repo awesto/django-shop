467703b9cf728f517c0c360b7bbb4034fc10ba4f
Jacob Rief <jacob.rief@gmail.com>
2019-02-19 10:21:18 +0100

    fix docstring

--- shop/serializers/defaults/product_select.py
+++ shop/serializers/defaults/product_select.py
@@ -7,8 +7,9 @@ from shop.models.product import ProductModel
 
 class ProductSelectSerializer(serializers.ModelSerializer):
     """
-    A simple serializer to convert the product's name and code for while rendering the `Select2 Widget`_
-    when looking up for a product. This serializer shall return a list of 2-tuples, whose 1st entry is the
+    A simple serializer to convert the product's name and code used for rendering the
+    `Select2 Widget`_'s content, while looking up for a certain product.
+    This serializer shall return a list of 2-tuples, whose 1st entry is the
     primary key of the product and the second entry is the rendered name.
 
     .. _Select2 Widget: https://github.com/applegrew/django-select2
