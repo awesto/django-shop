0cd32b80b27b24532b4ef1e802065d496a7147b4
Jacob Rief <jacob.rief@gmail.com>
2019-02-25 20:55:17 +0100

    Example now is handled by Cookiecutter

--- example/dumpdata.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-[ -z "$DJANGO_SHOP_TUTORIAL" ] && echo "Environment variable DJANGO_SHOP_TUTORIAL is not set" && exit 1
-mkdir -p ../workdir/$DJANGO_SHOP_TUTORIAL/fixtures
-./manage.py dumpdata --indent=2 --natural-foreign email_auth cms cmsplugin_cascade djangocms_text_ckeditor filer post_office shop myshop --exclude cmsplugin_cascade.segmentation --exclude filer.clipboard --exclude filer.clipboarditem --exclude myshop.order --exclude myshop.orderpayment --exclude myshop.orderitem --exclude myshop.cart --exclude myshop.cartitem > ../workdir/$DJANGO_SHOP_TUTORIAL/fixtures/myshop.json
--- example/myshop/admin/manufacturer.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from myshop.models.manufacturer import Manufacturer
-
-
-admin.site.register(Manufacturer, admin.ModelAdmin)
