aec5f69f6cfdf7f543d441c47c52cf6bb7b19bae
Jacob Rief <jacob.rief@gmail.com>
2019-03-02 00:58:31 +0100

    Fix docstrings

--- shop/serializers/defaults/product_summary.py
+++ shop/serializers/defaults/product_summary.py
@@ -7,15 +7,18 @@ from shop.serializers.bases import ProductSerializer
 
 class ProductSummarySerializer(ProductSerializer):
     """
-    Default serializer to create a summary from our Product model.
+    Default serializer to create a summary from our Product model. This summary then is used to
+    render various list views, such as the catalog- and the cart-, and the list of ordered items.
     In case the Product model is polymorphic, this shall serialize the smallest common denominator
     of all product information.
     """
     media = serializers.SerializerMethodField(
-        help_text="Returns a rendered HTML snippet containing a sample image among other elements")
+        help_text="Returns a rendered HTML snippet containing a sample image among other elements",
+    )
 
     caption = serializers.SerializerMethodField(
-        help_text="Returns the content from caption field if available")
+        help_text="Returns the content from caption field if available",
+    )
 
     class Meta(ProductSerializer.Meta):
         fields = ['id', 'product_name', 'product_url', 'product_model', 'price', 'media', 'caption']
