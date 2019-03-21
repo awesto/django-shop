dd4f0a836cff54e4f9e78bb30171dc1d912a0e9e
Jacob Rief <jacob.rief@gmail.com>
2019-03-21 14:51:39 +0100

    Inherit Meta from base class

--- shop/models/defaults/delivery.py
+++ shop/models/defaults/delivery.py
@@ -7,6 +7,6 @@ from shop.models.delivery import BaseDelivery
 
 class Delivery(BaseDelivery):
     """Default materialized model for OrderShipping"""
-    class Meta:
+    class Meta(BaseDelivery.Meta):
         verbose_name = _("Delivery")
         verbose_name_plural = _("Deliveries")
