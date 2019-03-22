560ec6a2b78c655fc645afaf742fc7f0dc800332
Jacob Rief <jacob.rief@gmail.com>
2019-03-22 11:39:07 +0100

    temporarily disable post_office requirement

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'Django>=1.11,<2.0',
-        'django-post-office>=3.0.0',
+        #'django-post-office>=3.0.0',
         'django-filer>=1.3.2',
         'django-ipware>=1.1.1',
         'django-fsm>=2.4.0',
