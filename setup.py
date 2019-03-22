e1eaa08840c5743daca70faf74c298b694edfdb6
Jacob Rief <jacob.rief@gmail.com>
2019-03-22 14:38:36 +0100

    attempt without version number

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'Django>=1.11,<2.0',
-        'django-post_office>=3.2',
+        'django-post_office',
         'django-filer>=1.3.2',
         'django-ipware>=1.1.1',
         'django-fsm>=2.4.0',
@@ -55,7 +55,7 @@ setup(
         'djangocms-cascade>=0.17.10',
     ],
     dependency_links=[
-        'https://github.com/jrief/django-post_office/archive/inlined-images-template-engine.zip#egg=django-post_office-3.2.0.dev',
+        'https://github.com/jrief/django-post_office/archive/inlined-images-template-engine.zip#egg=django-post_office',
     ],
     # Note: this requires setuptools >= 18.0.
     extras_require={
