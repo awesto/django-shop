05b5f2e6a016abcc9e9f920fa91a16eaf443a743
Jacob Rief <jacob.rief@gmail.com>
2019-05-10 14:57:37 +0200

    with inventory managment enabled render alternative template

--- shop/cascade/catalog.py
+++ shop/cascade/catalog.py
@@ -13,7 +13,7 @@ from cmsplugin_cascade.models import SortableInlineCascadeElement
 from cmsplugin_cascade.fields import GlossaryField
 from shop.cascade.plugin_base import ShopPluginBase, ProductSelectField
 from shop.conf import app_settings
-from shop.models.product import ProductModel
+from shop.models.product import ProductModel, AvailableProductMixin
 
 if is_installed('adminsortable2'):
     from adminsortable2.admin import SortableInlineAdminMixin
@@ -77,9 +77,13 @@ class ShopAddToCartPlugin(ShopPluginBase):
         templates = []
         if instance.glossary.get('render_template'):
             templates.append(instance.glossary['render_template'])
+        if isinstance(context['product'], AvailableProductMixin):
+            template_prefix = 'available-'
+        else:
+            template_prefix = ''
         templates.extend([
-            '{}/catalog/product-add2cart.html'.format(app_settings.APP_LABEL),
-            'shop/catalog/product-add2cart.html',
+            '{}/catalog/{}product-add2cart.html'.format(app_settings.APP_LABEL, template_prefix),
+            'shop/catalog/{}product-add2cart.html'.format(template_prefix),
         ])
         return select_template(templates)
 
