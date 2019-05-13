b53aeb3865587a6a44abb93b9e5af136131d8f01
Jacob Rief <jacob.rief@gmail.com>
2019-05-14 00:07:18 +0200

    swap lines of code

--- shop/models/order.py
+++ shop/models/order.py
@@ -552,8 +552,8 @@ class BaseOrderItem(with_metaclass(deferred.ForeignKeyBuilder, models.Model)):
         """
         if cart_item.quantity == 0:
             raise CartItemModel.DoesNotExist("Cart Item is on the Wish List")
+        cart_item.product.deduct_from_stock(cart_item.quantity, **cart_item.extra)
         self.product = cart_item.product
-        self.product.deduct_from_stock(cart_item.quantity, **cart_item.extra)
         # for historical integrity, store the product's name and price at the moment of purchase
         self.product_name = cart_item.product.product_name
         self.product_code = cart_item.product_code
