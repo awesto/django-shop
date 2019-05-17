3ed79f4e41e04003cbd01c1150d30396cf16c341
Jacob Rief <jacob.rief@gmail.com>
2019-05-17 15:09:52 +0200

    readability counts

--- shop/models/product.py
+++ shop/models/product.py
@@ -27,15 +27,27 @@ class Availability(object):
     """
     def __init__(self, **kwargs):
         """
-        :param earliest: Point in time from when this product will be available.
-        :param latest: Point in time until this product will be available.
-        :param quantity: Number of available items.
-        :param sell_short: Sell the product, even though it's not in stock. It
-            will be shipped at the ``earliest`` point in time.
-        :param limited_offer: Sell the product until the ``latest`` point in time.
+        :param earliest:
+            Point in time from when on this product will be available.
+
+        :param latest:
+            Point in time until this product will be available.
+
+        :param quantity:
+            Number of available items. The type of this value is the same as the type of ``quantity``
+            in :class:`shop.models.cart.CartItemModel`.
+
+        :param sell_short:
+            If ``True``, sell the product even though it's not in stock. It then will be shipped
+            at the point in time specified by ``earliest``.
+
+        :param limited_offer:
+            If ``True``, sell the product until the point in time specified by ``latest``. After
+            that period, the product will not be available anymore.
         """
-        self.earliest = kwargs.get('earliest', timezone.datetime.min.replace(tzinfo=timezone.get_current_timezone()))
-        self.latest = kwargs.get('latest', timezone.datetime.max.replace(tzinfo=timezone.get_current_timezone()))
+        tzinfo = timezone.get_current_timezone()
+        self.earliest = kwargs.get('earliest', timezone.datetime.min.replace(tzinfo=tzinfo))
+        self.latest = kwargs.get('latest', timezone.datetime.max.replace(tzinfo=tzinfo))
         quantity = kwargs.get('quantity', app_settings.MAX_PURCHASE_QUANTITY)
         self.quantity = min(quantity, app_settings.MAX_PURCHASE_QUANTITY)
         self.sell_short = bool(kwargs.get('sell_short', False))
