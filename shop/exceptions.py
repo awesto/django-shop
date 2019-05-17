dcdd00fc5eb8af75cded7469e4af3a9853d88c44
Jacob Rief <jacob.rief@gmail.com>
2019-05-17 12:21:59 +0200

    No translations in exceptions

--- shop/exceptions.py
+++ shop/exceptions.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.utils.translation import ugettext_lazy as _
-
 
 class ProductNotAvailable(Exception):
     """
     The product being purchased isn't available anymore.
     """
     def __init__(self, product):
-        msg = _("Product {} isn't available anymore.")
+        self.product = product
+        msg = "Product {} isn't available anymore."
         super(ProductNotAvailable, self).__init__(msg.format(product.product_code))
