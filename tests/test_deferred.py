007504f2eece9954fb0d907610bb7b5d6a2515f6
Haricot <np.pascal@gmail.com>
2019-07-12 22:26:48 +0200

    update

--- tests/test_deferred.py
+++ tests/test_deferred.py
@@ -16,7 +16,6 @@ import six
 def create_regular_class(name, fields={}, meta={}):
     meta.setdefault('app_label', 'foo')
     Meta = type(str('Meta'), (), meta)
-    field = {'on_delete', models.CASCADE}
     return type(str(name), (models.Model,), dict(Meta=Meta, __module__=__name__, **fields))
 
 
