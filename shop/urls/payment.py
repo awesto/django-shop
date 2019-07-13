348a9e53fba03336a503bcd7c519ccdf2f1d69c1
Haricot <np.pascal@gmail.com>
2019-07-13 10:33:03 +0200

    update

--- shop/urls/payment.py
+++ shop/urls/payment.py
@@ -15,7 +15,8 @@ for modifier in cart_modifiers_pool.get_payment_modifiers():
         path_ = '{}/'.format(namespace)
         urls = modifier.payment_provider.get_urls()
         provider_url = path( path_, include((urls, 'url_payment'), namespace=namespace))
-        if provider_url.urlpatterns != []:
+        if provider_url.url_patterns != []:
             urlpatterns.append(provider_url)
     except AttributeError as err:
-        warnings.warn(str(err))
+        warnings.warn(err.message)
+        
