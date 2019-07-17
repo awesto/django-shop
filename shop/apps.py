ea4fc0485fafe566e8a39cae401abc4f5efc34be
Nicolas PASCAL <np.pascal@gmail.com>
2019-07-17 07:57:28 +0200

    Update apps.py

--- shop/apps.py
+++ shop/apps.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class ShopConfig(AppConfig):
