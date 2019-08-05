f9e0879c95b26eaa317309c6141635bd96b38f75
Nicolas PASCAL <np.pascal@gmail.com>
2019-08-05 09:03:52 +0200

    divides class Customer to avoid duplication

--- shop/models/defaults/customer.py
+++ shop/models/defaults/customer.py
@@ -7,14 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 from shop.conf import app_settings
 from shop.models.customer import BaseCustomer
 
-
-class Customer(BaseCustomer):
-    """
-    Default materialized model for Customer, adding a customer's number and salutation.
-
-    If this model is materialized, then also register the corresponding serializer class
-    :class:`shop.serializers.defaults.customer.CustomerSerializer`.
-    """
+class CustomerBase(models.Model):
     SALUTATION = [('mrs', _("Mrs.")), ('mr', _("Mr.")), ('na', _("(n/a)"))]
     number = models.PositiveIntegerField(
         _("Customer Number"),
@@ -39,10 +32,13 @@ class Customer(BaseCustomer):
             self.save()
         return self.get_number()
 
-    def as_text(self):
-        template_names = [
-            '{}/customer.txt'.format(app_settings.APP_LABEL),
-            'shop/customer.txt',
-        ]
-        template = select_template(template_names)
-        return template.render({'customer': self})
+    @classmethod
+    def reorder_form_fields(self, field_order):
+        field_order.insert(0, 'salutation')
+        return field_order
+        
+    class Meta:
+        abstract = True
+
+class Customer(CustomerBase, BaseCustomer):
+    pass
