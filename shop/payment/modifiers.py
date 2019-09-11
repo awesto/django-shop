7659bb0e77c1f4d312a7b65976932e0a22d2a85c
Nicolas PASCAL <np.pascal@gmail.com>
2019-09-12 00:23:01 +0200

    Update modifiers.py

--- shop/payment/modifiers.py
+++ shop/payment/modifiers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from shop.modifiers.base import BaseCartModifier
 from shop.payment.providers import PaymentProvider, ForwardFundPayment
