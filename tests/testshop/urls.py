70998a158323567e80c7460422d29966abfc8b9c
Nicolas PASCAL <np.pascal@gmail.com>
2019-09-12 01:14:16 +0200

    Update urls.py

--- tests/testshop/urls.py
+++ tests/testshop/urls.py
@@ -1,11 +1,11 @@
-from django.urls import include, path, re_path
+from django.conf.urls import url, include
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 urlpatterns = [
-    path('shop/', include(('shop.urls', 'shop_urls' ), namespace='shop')),
+    url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
 urlpatterns.extend(i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('', include('cms.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
 ))
