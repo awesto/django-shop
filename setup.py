278726aa91d7b3797b687df0f0c09597ce899910
Nicolas PASCAL <np.pascal@gmail.com>
2019-09-11 18:57:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,12 +54,8 @@ setup(
         'django-admin-sortable2>=0.6.19',
         'django-formtools>=1.0',
        # 'djangocms-cascade>=0.18.2',
-       # 'djangocms-cascade @ git+ssh://git@github.com/jrief/djangocms-cascade.git@releases/1.0.x#egg=djangocms-cascade',
     ],
-    #dependency_links=['github.com/jrief/djangocms-cascade/releases/1.0.x#egg=djangocms-cascade'],
-    dependency_links=['github.com/haricot/djangocms-cascade/wip_shop_django2#egg=djangocms-cascade-wip_shop_django2',
-        'https://github.com/haricot/djangocms-bootstrap3/wip_django2/#egg=djangocms-cascade-wip_django2'
-    ],
+    dependency_links=['github.com/jrief/djangocms-cascade/releases/1.0.x#egg=djangocms-cascade'],
     # Note: this requires setuptools >= 18.0.
     extras_require={
         ':python_version<"3.4"': ['enum34'],
