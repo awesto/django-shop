04902823cfcd99aa1875bfec4bf191d305512cfc
Nicolas PASCAL <np.pascal@gmail.com>
2019-09-12 00:26:41 +0200

    Update modifiers.py

--- shop/shipping/modifiers.py
+++ shop/shipping/modifiers.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.utils import timezone
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from shop.modifiers.base import BaseCartModifier
 
 
