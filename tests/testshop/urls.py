2390b857599fe17188f2d389778eafed1532c518
Nicolas PASCAL <np.pascal@gmail.com>
2019-09-12 01:49:53 +0200

    add logic urls if DJANGO_VERSION

--- tests/testshop/urls.py
+++ tests/testshop/urls.py
@@ -1,11 +1,24 @@
-from django.urls import include, path, re_path
+from django import VERSION as DJANGO_VERSION
+if DJANGO_VERSION < (2, 0):
+   from django.conf.urls import url, include
+esle:
+    from django.urls import include, path, re_path
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-urlpatterns = [
-    path('shop/', include(('shop.urls', 'shop_urls' ), namespace='shop')),
-]
-urlpatterns.extend(i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('', include('cms.urls')),
-))
+if DJANGO_VERSION < (2, 0):
+    urlpatterns = [
+        url(r'^shop/', include('shop.urls', namespace='shop')),
+    ]
+    urlpatterns.extend(i18n_patterns(
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^', include('cms.urls')),
+    ))
+esle:
+    urlpatterns = [
+        path('shop/', include(('shop.urls', 'shop_urls' ), namespace='shop')),
+    ]
+    urlpatterns.extend(i18n_patterns(
+        path('admin/', admin.site.urls),
+        path('', include('cms.urls')),
+    ))
