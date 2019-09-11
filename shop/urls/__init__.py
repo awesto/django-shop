225c3f6a3ef937b182056cd415a96c6eed409863
Nicolas PASCAL <np.pascal@gmail.com>
2019-09-12 01:40:26 +0200

    Update __init__.py

--- shop/urls/__init__.py
+++ shop/urls/__init__.py
@@ -6,8 +6,6 @@ from shop.urls import rest_api
 from shop.urls import auth
 from shop.urls import payment
 
-app_name = 'shop'
-
 urlpatterns = [
     url(r'^api/', include(rest_api)),
     url(r'^auth/', include(auth)),
