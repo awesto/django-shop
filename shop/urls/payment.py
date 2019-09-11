52b7a55aabc5e1139ad0dc492849d5848abff08c
Nicolas PASCAL <np.pascal@gmail.com>
2019-09-12 00:47:04 +0200

    Update payment.py

--- shop/urls/payment.py
+++ shop/urls/payment.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import warnings
-from django.urls import include, path, re_path
+from django.conf.urls import include, url
 from shop.modifiers.pool import cart_modifiers_pool
 
 
 urlpatterns = []
-
+# For every payment modifier, load the URLs from the associated `payment_provider`
 for modifier in cart_modifiers_pool.get_payment_modifiers():
     try:
         namespace = modifier.payment_provider.namespace
-       # regexp = r'^{}/'.format(namespace)
-        path_ = '{}/'.format(namespace)
+        regexp = r'^{}/'.format(namespace)
         urls = modifier.payment_provider.get_urls()
-        provider_url = path( path_, include((urls, 'url_payment'), namespace=namespace))
-        if provider_url.url_patterns != []:
+        provider_url = url(regexp, include(urls, namespace=namespace))
+        if provider_url.describe() not in [u.describe() for u in urlpatterns]:
             urlpatterns.append(provider_url)
     except AttributeError as err:
         warnings.warn(err.message)
