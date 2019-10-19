2bf93989f5b97921861250c85e9ebd3b90c2b7e9
Nicolas PASCAL <np.pascal@gmail.com>
2019-10-19 12:45:32 +0200

    drop support django111

--- shop/admin/customer.py
+++ shop/admin/customer.py
@@ -9,11 +9,8 @@ from django.contrib import admin
 from django.utils.encoding import force_str
 from django.utils.timezone import localtime
 from django.utils.translation import pgettext_lazy, ugettext_lazy as _
-from django import VERSION as DJANGO_VERSION
 
 from shop.models.customer import CustomerModel, CustomerState
-
-DJANGO111 = True if DJANGO_VERSION < (2, 0) else False
 
 class CustomerInlineAdminBase(admin.StackedInline):
     model = CustomerModel
@@ -130,11 +127,7 @@ class CustomerAdminBase(UserAdmin):
 
     def is_unexpired(self, user):
         if hasattr(user, 'customer'):
-
-            if DJANGO111:
-                return not user.customer.is_expired()
-            else:
-                return not user.customer.is_expired
+            return not user.customer.is_expired
         return True
     is_unexpired.short_description = _("Unexpired")
     is_unexpired.boolean = True
