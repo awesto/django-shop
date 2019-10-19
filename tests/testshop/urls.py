73968d2c71285776642abcb2a4bfdbfdb738e1a7
Nicolas PASCAL <np.pascal@gmail.com>
2019-10-19 19:36:12 +0200

    Update urls.py

--- tests/testshop/urls.py
+++ tests/testshop/urls.py
@@ -1,24 +1,11 @@
-from django import VERSION as DJANGO_VERSION
-if DJANGO_VERSION < (2, 0):
-    from django.conf.urls import url, include
-else:
-    from django.urls import include, path, re_path
+from django.urls import include, path, re_path
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-if DJANGO_VERSION < (2, 0):
-    urlpatterns = [
-        url(r'^shop/', include('shop.urls', namespace='shop')),
-    ]
-    urlpatterns.extend(i18n_patterns(
-        url(r'^admin/', include(admin.site.urls)),
-        url(r'^', include('cms.urls')),
-    ))
-else:
-    urlpatterns = [
-        path('shop/', include(('shop.urls', 'shop_urls' ), namespace='shop')),
-    ]
-    urlpatterns.extend(i18n_patterns(
-        path('admin/', admin.site.urls),
-        path('', include('cms.urls')),
-    ))
+urlpatterns = [
+    path('shop/', include(('shop.urls', 'shop_urls' ), namespace='shop')),
+]
+urlpatterns.extend(i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('cms.urls')),
+))
