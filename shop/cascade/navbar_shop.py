1a0f32fc6c8e3faa9b2781cdd722c740e638f913
Haricot <np.pascal@gmail.com>
2020-03-16 12:47:51 +0100

    update navbar_shop settings cart-caddy.html

--- shop/cascade/navbar_shop.py
+++ shop/cascade/navbar_shop.py
@@ -16,11 +16,19 @@ from shop.models.product import ProductModel
 class ShopNavbarLoginLogout(ShopPluginBase):
     name = _("Nav Login-Logout")
     require_parent = True
-#    form = ShopCatalogPluginForm
     parent_classes = ['BootstrapListsPlugin']
     cache = False
     render_template = 'shop/navbar/login-logout.html'
 
+    @classmethod
+    def get_identifier(cls, obj):
+        identifier = super().get_identifier(obj)
+        if hasattr(cls,'default_css_class'):
+            css_classes_without_default = obj.css_classes.replace( cls.default_css_class , '' , 1)
+        else:
+            css_classes_without_default = obj.css_classes
+        return format_html('<div style="font-size: smaller; white-space: pre-wrap;" >{0}{1}</div>',
+        identifier, css_classes_without_default )
 
 @plugin_pool.register_plugin
 class ShopNavbarWatchList(ShopPluginBase):
@@ -29,6 +37,16 @@ class ShopNavbarWatchList(ShopPluginBase):
     parent_classes = ['BootstrapListsPlugin']
     cache = False
     render_template = 'shop/navbar/watch-icon.html'
+
+    @classmethod
+    def get_identifier(cls, obj):
+        identifier = super().get_identifier(obj)
+        if hasattr(cls,'default_css_class'):
+            css_classes_without_default = obj.css_classes.replace( cls.default_css_class , '' , 1)
+        else:
+            css_classes_without_default = obj.css_classes
+        return format_html('<div style="font-size: smaller; white-space: pre-wrap;" >{0}{1}</div>',
+        identifier, css_classes_without_default )
 
 
 @plugin_pool.register_plugin
@@ -39,6 +57,16 @@ class ShopNavbarCart(ShopPluginBase):
     cache = False
     render_template = 'shop/navbar/cart-caddy.html'
 
+    @classmethod
+    def get_identifier(cls, obj):
+        identifier = super().get_identifier(obj)
+        if hasattr(cls,'default_css_class'):
+            css_classes_without_default = obj.css_classes.replace( cls.default_css_class , '' , 1)
+        else:
+            css_classes_without_default = obj.css_classes
+        return format_html('<div style="font-size: smaller; white-space: pre-wrap;" >{0}{1}</div>',
+        identifier, css_classes_without_default )
+
 
 @plugin_pool.register_plugin
 class ShopNavbarSearchForm(ShopPluginBase):
@@ -48,3 +76,13 @@ class ShopNavbarSearchForm(ShopPluginBase):
     cache = False
     render_template = 'shop/navbar/search-form.html'
 
+
+    @classmethod
+    def get_identifier(cls, obj):
+        identifier = super().get_identifier(obj)
+        if hasattr(cls,'default_css_class'):
+            css_classes_without_default = obj.css_classes.replace( cls.default_css_class , '' , 1)
+        else:
+            css_classes_without_default = obj.css_classes
+        return format_html('<div style="font-size: smaller; white-space: pre-wrap;" >{0}{1}</div>',
+        identifier, css_classes_without_default )
--- shop/cascade/settings.py
+++ shop/cascade/settings.py
@@ -36,13 +36,21 @@ def set_defaults(config):
     config['plugins_with_extra_mixins'].setdefault('BootstrapButtonPlugin', BootstrapUtilities(
         BootstrapUtilities.floats,
     ))
+
     config['plugins_with_extra_mixins'].setdefault('ShopNavbarSearchForm', BootstrapUtilities(
         BootstrapUtilities.margins,
     ))
+    ShopNavbarCart
     config['plugins_with_extra_fields'].setdefault('ShopNavbarSearchForm', PluginExtraFieldsConfig(
         css_classes={
             'multiple': True,
             'class_names': ['shop-search-form'],
+        },
+    ))
+    config['plugins_with_extra_mixins'].setdefault('ShopNavbarCart', PluginExtraFieldsConfig(
+        css_classes={
+            'multiple': True,
+            'class_names': ['shop-secondary-menu'],
         },
     ))
     config['plugins_with_extra_fields'].setdefault('BootstrapNavBrandPlugin', PluginExtraFieldsConfig(
