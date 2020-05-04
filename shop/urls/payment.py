00ee6d734d80899dc7347db435c1c3562c9f4969
Jacob Rief <jacob.rief@gmail.com>
2020-05-04 20:05:44 +0200

    drop support for Django-1.11, Python-3.5, add Python-3.8

--- shop/urls/payment.py
+++ shop/urls/payment.py
@@ -11,17 +11,10 @@ urlpatterns = []
 for modifier in cart_modifiers_pool.get_payment_modifiers():
     try:
         namespace = modifier.payment_provider.namespace
-        if DJANGO_VERSION < (2, 0):
-            regexp = r'^{}/'.format(namespace)
-            urls = modifier.payment_provider.get_urls()
-            provider_url = url(regexp, include(urls, namespace=namespace))
-            if provider_url.describe() not in [u.describe() for u in urlpatterns]:
-                urlpatterns.append(provider_url)
-        else:
-            path_ = '{}/'.format(namespace)
-            urls = modifier.payment_provider.get_urls()
-            provider_url = path( path_, include((urls, 'url_payment'), namespace=namespace))
-            if provider_url.url_patterns != []:
-                urlpatterns.append(provider_url)
+        path_ = '{}/'.format(namespace)
+        urls = modifier.payment_provider.get_urls()
+        provider_url = path( path_, include((urls, 'url_payment'), namespace=namespace))
+        if provider_url.url_patterns != []:
+            urlpatterns.append(provider_url)
     except AttributeError as err:
         warnings.warn(err.message)
