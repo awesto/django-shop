79a60ec07b226f0af176cfafcce90045b5636f7b
Morteza NourelahiAlamdari <m@0t1.me>
2021-12-14 19:34:17 +0100

    Refactoring init

--- shop/models/cart.py
+++ shop/models/cart.py
@@ -62,87 +62,108 @@ class CartItemManager(models.Manager):
         return watch_items
 
 
-class BaseCartItem(models.Model, metaclass=deferred.ForeignKeyBuilder):
+# class BaseCartItem(models.Model, metaclass=deferred.ForeignKeyBuilder):
+class BaseCartItem(models.Model):
     """
     This is a holder for the quantity of items in the cart and, obviously, a
     pointer to the actual Product being purchased
     """
-    cart = deferred.ForeignKey(
+    # cart = deferred.ForeignKey(
+    #     'BaseCart',
+    #     on_delete=models.CASCADE,
+    #     related_name='items',
+    # )
+    #
+    # product = deferred.ForeignKey(
+    #     BaseProduct,
+    #     on_delete=models.CASCADE,
+    # )
+    #
+    # product_code = models.CharField(
+    #     _("Product code"),
+    #     max_length=255,
+    #     null=True,
+    #     blank=True,
+    #     help_text=_("Product code of added item."),
+    # )
+    #
+    # updated_at = models.DateTimeField(
+    #     _("Updated at"),
+    #     auto_now=True,
+    # )
+    #
+    # extra = JSONField(verbose_name=_("Arbitrary information for this cart item"))
+
+    cart = models.ForeignKey(
         'BaseCart',
         on_delete=models.CASCADE,
-        related_name='items',
     )
 
-    product = deferred.ForeignKey(
+    product = models.ForeignKey(
         BaseProduct,
         on_delete=models.CASCADE,
     )
 
     product_code = models.CharField(
-        _("Product code"),
         max_length=255,
         null=True,
         blank=True,
-        help_text=_("Product code of added item."),
     )
 
-    updated_at = models.DateTimeField(
-        _("Updated at"),
-        auto_now=True,
-    )
+    updated_at = models.DateTimeField(auto_now=True)
 
-    extra = JSONField(verbose_name=_("Arbitrary information for this cart item"))
+    extra = JSONField()
 
     objects = CartItemManager()
 
     class Meta:
         abstract = True
-        verbose_name = _("Cart item")
-        verbose_name_plural = _("Cart items")
-
-    @classmethod
-    def check(cls, **kwargs):
-        errors = super().check(**kwargs)
-        allowed_types = ['IntegerField', 'SmallIntegerField', 'PositiveIntegerField',
-                         'PositiveSmallIntegerField', 'DecimalField', 'FloatField']
-        for field in cls._meta.fields:
-            if field.attname == 'quantity':
-                if field.get_internal_type() not in allowed_types:
-                    msg = "Class `{}.quantity` must be of one of the types: {}."
-                    errors.append(checks.Error(msg.format(cls.__name__, allowed_types)))
-                break
-        else:
-            msg = "Class `{}` must implement a field named `quantity`."
-            errors.append(checks.Error(msg.format(cls.__name__)))
-        return errors
-
-    def __init__(self, *args, **kwargs):
-        # reduce the given fields to what the model actually can consume
-        all_field_names = [field.name for field in self._meta.get_fields(include_parents=True)]
-        model_kwargs = {k: v for k, v in kwargs.items() if k in all_field_names}
-        super().__init__(*args, **model_kwargs)
-        self.extra_rows = OrderedDict()
-        self._dirty = True
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        self.cart.save(update_fields=['updated_at'])
-        self._dirty = True
-
-    def update(self, request):
-        """
-        Loop over all registered cart modifier, change the price per cart item and optionally add
-        some extra rows.
-        """
-        if not self._dirty:
-            return
-        self.refresh_from_db()
-        self.extra_rows = OrderedDict()  # reset the dictionary
-        for modifier in cart_modifiers_pool.get_all_modifiers():
-            modifier.process_cart_item(self, request)
-        self._dirty = False
-
-CartItemModel = deferred.MaterializedModel(BaseCartItem)
+        # verbose_name = _("Cart item")
+        # verbose_name_plural = _("Cart items")
+#
+#     @classmethod
+#     def check(cls, **kwargs):
+#         errors = super().check(**kwargs)
+#         allowed_types = ['IntegerField', 'SmallIntegerField', 'PositiveIntegerField',
+#                          'PositiveSmallIntegerField', 'DecimalField', 'FloatField']
+#         for field in cls._meta.fields:
+#             if field.attname == 'quantity':
+#                 if field.get_internal_type() not in allowed_types:
+#                     msg = "Class `{}.quantity` must be of one of the types: {}."
+#                     errors.append(checks.Error(msg.format(cls.__name__, allowed_types)))
+#                 break
+#         else:
+#             msg = "Class `{}` must implement a field named `quantity`."
+#             errors.append(checks.Error(msg.format(cls.__name__)))
+#         return errors
+#
+#     def __init__(self, *args, **kwargs):
+#         # reduce the given fields to what the model actually can consume
+#         all_field_names = [field.name for field in self._meta.get_fields(include_parents=True)]
+#         model_kwargs = {k: v for k, v in kwargs.items() if k in all_field_names}
+#         super().__init__(*args, **model_kwargs)
+#         self.extra_rows = OrderedDict()
+#         self._dirty = True
+#
+#     def save(self, *args, **kwargs):
+#         super().save(*args, **kwargs)
+#         self.cart.save(update_fields=['updated_at'])
+#         self._dirty = True
+#
+#     def update(self, request):
+#         """
+#         Loop over all registered cart modifier, change the price per cart item and optionally add
+#         some extra rows.
+#         """
+#         if not self._dirty:
+#             return
+#         self.refresh_from_db()
+#         self.extra_rows = OrderedDict()  # reset the dictionary
+#         for modifier in cart_modifiers_pool.get_all_modifiers():
+#             modifier.process_cart_item(self, request)
+#         self._dirty = False
+#
+# CartItemModel = deferred.MaterializedModel(BaseCartItem)
 
 
 class CartManager(models.Manager):
@@ -170,99 +191,108 @@ class CartManager(models.Manager):
         return request._cached_cart
 
 
-class BaseCart(models.Model, metaclass=deferred.ForeignKeyBuilder):
+# class BaseCart(models.Model, metaclass=deferred.ForeignKeyBuilder):
+class BaseCart(models.Model):
     """
     The fundamental part of a shopping cart.
     """
-    customer = deferred.OneToOneField(
+    # customer = deferred.OneToOneField(
+    #     'BaseCustomer',
+    #     on_delete=models.CASCADE,
+    #     related_name='cart',
+    #     verbose_name=_("Customer"),
+    # )
+
+    customer = models.OneToOneField(
         'BaseCustomer',
         on_delete=models.CASCADE,
-        related_name='cart',
-        verbose_name=_("Customer"),
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    extra = JSONField()
 
-    created_at = models.DateTimeField(
-        _("Created at"),
-        auto_now_add=True,
-    )
+    # created_at = models.DateTimeField(
+    #     _("Created at"),
+    #     auto_now_add=True,
+    # )
+    #
+    # updated_at = models.DateTimeField(
+    #     _("Updated at"),
+    #     auto_now=True,
+    # )
 
-    updated_at = models.DateTimeField(
-        _("Updated at"),
-        auto_now=True,
-    )
-
-    extra = JSONField(verbose_name=_("Arbitrary information for this cart"))
+    # extra = JSONField(verbose_name=_("Arbitrary information for this cart"))
 
     # our CartManager determines the cart object from the request.
     objects = CartManager()
 
     class Meta:
         abstract = True
-        verbose_name = _("Shopping Cart")
-        verbose_name_plural = _("Shopping Carts")
+        # verbose_name = _("Shopping Cart")
+        # verbose_name_plural = _("Shopping Carts")
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # That will hold things like tax totals or total discount
-        self.extra_rows = OrderedDict()
-        self._cached_cart_items = None
-        self._dirty = True
-
-    def save(self, force_update=False, *args, **kwargs):
-        if self.pk or force_update is False:
-            super().save(force_update=force_update, *args, **kwargs)
-        self._dirty = True
-
-    def update(self, request, raise_exception=False):
-        """
-        This should be called after a cart item changed quantity, has been added or removed.
-
-        It will loop over all items in the cart, and call all the configured cart modifiers.
-        After this is done, it will compute and update the order's total and subtotal fields, along
-        with any supplement added along the way by modifiers.
-
-        Note that theses added fields are not stored - we actually want to
-        reflect rebate and tax changes on the *cart* items, but we don't want
-        that for the order items (since they are legally binding after the
-        "purchase" button was pressed)
-        """
-        if not self._dirty:
-            return
-
-        if self._cached_cart_items:
-            items = self._cached_cart_items
-        else:
-            items = CartItemModel.objects.filter_cart_items(self, request)
-
-        # This calls all the pre_process_cart methods and the pre_process_cart_item for each item,
-        # before processing the cart. This allows to prepare and collect data on the cart.
-        for modifier in cart_modifiers_pool.get_all_modifiers():
-            modifier.pre_process_cart(self, request, raise_exception)
-            for item in items:
-                modifier.pre_process_cart_item(self, item, request, raise_exception)
-
-        self.extra_rows = OrderedDict()  # reset the dictionary
-        self.subtotal = 0  # reset the subtotal
-        for item in items:
-            # item.update iterates over all cart modifiers and invokes method `process_cart_item`
-            item.update(request)
-            self.subtotal += item.line_total
-
-        # Iterate over the registered modifiers, to process the cart's summary
-        for modifier in cart_modifiers_pool.get_all_modifiers():
-            for item in items:
-                modifier.post_process_cart_item(self, item, request)
-            modifier.process_cart(self, request)
-
-        # This calls the post_process_cart method from cart modifiers, if any.
-        # It allows for a last bit of processing on the "finished" cart, before
-        # it is displayed
-        for modifier in reversed(cart_modifiers_pool.get_all_modifiers()):
-            modifier.post_process_cart(self, request)
-
-        # Cache updated cart items
-        self._cached_cart_items = items
-        self._dirty = False
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     # That will hold things like tax totals or total discount
+    #     self.extra_rows = OrderedDict()
+    #     self._cached_cart_items = None
+    #     self._dirty = True
+    #
+    # def save(self, force_update=False, *args, **kwargs):
+    #     if self.pk or force_update is False:
+    #         super().save(force_update=force_update, *args, **kwargs)
+    #     self._dirty = True
+    #
+    # def update(self, request, raise_exception=False):
+    #     """
+    #     This should be called after a cart item changed quantity, has been added or removed.
+    #
+    #     It will loop over all items in the cart, and call all the configured cart modifiers.
+    #     After this is done, it will compute and update the order's total and subtotal fields, along
+    #     with any supplement added along the way by modifiers.
+    #
+    #     Note that theses added fields are not stored - we actually want to
+    #     reflect rebate and tax changes on the *cart* items, but we don't want
+    #     that for the order items (since they are legally binding after the
+    #     "purchase" button was pressed)
+    #     """
+    #     if not self._dirty:
+    #         return
+    #
+    #     if self._cached_cart_items:
+    #         items = self._cached_cart_items
+    #     else:
+    #         items = CartItemModel.objects.filter_cart_items(self, request)
+    #
+    #     # This calls all the pre_process_cart methods and the pre_process_cart_item for each item,
+    #     # before processing the cart. This allows to prepare and collect data on the cart.
+    #     for modifier in cart_modifiers_pool.get_all_modifiers():
+    #         modifier.pre_process_cart(self, request, raise_exception)
+    #         for item in items:
+    #             modifier.pre_process_cart_item(self, item, request, raise_exception)
+    #
+    #     self.extra_rows = OrderedDict()  # reset the dictionary
+    #     self.subtotal = 0  # reset the subtotal
+    #     for item in items:
+    #         # item.update iterates over all cart modifiers and invokes method `process_cart_item`
+    #         item.update(request)
+    #         self.subtotal += item.line_total
+    #
+    #     # Iterate over the registered modifiers, to process the cart's summary
+    #     for modifier in cart_modifiers_pool.get_all_modifiers():
+    #         for item in items:
+    #             modifier.post_process_cart_item(self, item, request)
+    #         modifier.process_cart(self, request)
+    #
+    #     # This calls the post_process_cart method from cart modifiers, if any.
+    #     # It allows for a last bit of processing on the "finished" cart, before
+    #     # it is displayed
+    #     for modifier in reversed(cart_modifiers_pool.get_all_modifiers()):
+    #         modifier.post_process_cart(self, request)
+    #
+    #     # Cache updated cart items
+    #     self._cached_cart_items = items
+    #     self._dirty = False
 
     def empty(self):
         """
@@ -272,24 +302,24 @@ class BaseCart(models.Model, metaclass=deferred.ForeignKeyBuilder):
             self.items.all().delete()
             self.delete()
 
-    def merge_with(self, other_cart):
-        """
-        Merge the contents of the other cart into this one, afterwards delete it.
-        This is done item by item, so that duplicate items increase the quantity.
-        """
-        # iterate over the cart and add quantities for items from other cart considered as equal
-        if self.id == other_cart.id:
-            raise RuntimeError("Can not merge cart with itself")
-        for item in self.items.all():
-            other_item = item.product.is_in_cart(other_cart, extra=item.extra)
-            if other_item:
-                item.quantity += other_item.quantity
-                item.save()
-                other_item.delete()
-
-        # the remaining items from the other cart are merged into this one
-        other_cart.items.update(cart=self)
-        other_cart.delete()
+    # def merge_with(self, other_cart):
+    #     """
+    #     Merge the contents of the other cart into this one, afterwards delete it.
+    #     This is done item by item, so that duplicate items increase the quantity.
+    #     """
+    #     # iterate over the cart and add quantities for items from other cart considered as equal
+    #     if self.id == other_cart.id:
+    #         raise RuntimeError("Can not merge cart with itself")
+    #     for item in self.items.all():
+    #         other_item = item.product.is_in_cart(other_cart, extra=item.extra)
+    #         if other_item:
+    #             item.quantity += other_item.quantity
+    #             item.save()
+    #             other_item.delete()
+    #
+    #     # the remaining items from the other cart are merged into this one
+    #     other_cart.items.update(cart=self)
+    #     other_cart.delete()
 
     def __str__(self):
         return "{}".format(self.pk) if self.pk else "(unsaved)"
@@ -315,14 +345,14 @@ class BaseCart(models.Model, metaclass=deferred.ForeignKeyBuilder):
     def is_empty(self):
         return self.num_items == 0 and self.total_quantity == 0
 
-    def get_caption_data(self):
-        warnings.warn("This method is deprecated")
-        return {'num_items': self.num_items, 'total_quantity': self.total_quantity,
-                'subtotal': self.subtotal, 'total': self.total}
+    # def get_caption_data(self):
+    #     warnings.warn("This method is deprecated")
+    #     return {'num_items': self.num_items, 'total_quantity': self.total_quantity,
+    #             'subtotal': self.subtotal, 'total': self.total}
 
-    @classmethod
-    def get_default_caption_data(cls):
-        warnings.warn("This method is deprecated")
-        return {'num_items': 0, 'total_quantity': 0, 'subtotal': Money(), 'total': Money()}
+    # @classmethod
+    # def get_default_caption_data(cls):
+    #     warnings.warn("This method is deprecated")
+    #     return {'num_items': 0, 'total_quantity': 0, 'subtotal': Money(), 'total': Money()}
 
-CartModel = deferred.MaterializedModel(BaseCart)
+# CartModel = deferred.MaterializedModel(BaseCart)
--- shop/models/defaults/cart.py
+++ shop/models/defaults/cart.py
@@ -1,6 +1,7 @@
 from django.db.models import SET_DEFAULT
 
-from shop import deferred
+# from shop import deferred
+from django.db import models
 from shop.models.address import BaseShippingAddress, BaseBillingAddress
 from shop.models.cart import BaseCart
 
@@ -9,18 +10,32 @@ class Cart(BaseCart):
     """
     Default materialized model for BaseCart containing common fields
     """
-    shipping_address = deferred.ForeignKey(
+    # shipping_address = deferred.ForeignKey(
+    #     BaseShippingAddress,
+    #     on_delete=SET_DEFAULT,
+    #     null=True,
+    #     default=None,
+    #     related_name='+',
+    # )
+    #
+    # billing_address = deferred.ForeignKey(
+    #     BaseBillingAddress,
+    #     on_delete=SET_DEFAULT,
+    #     null=True,
+    #     default=None,
+    #     related_name='+',
+    # )
+
+    shipping_address = models.ForeignKey(
         BaseShippingAddress,
         on_delete=SET_DEFAULT,
         null=True,
         default=None,
-        related_name='+',
     )
 
-    billing_address = deferred.ForeignKey(
+    billing_address = models.ForeignKey(
         BaseBillingAddress,
         on_delete=SET_DEFAULT,
         null=True,
         default=None,
-        related_name='+',
     )
